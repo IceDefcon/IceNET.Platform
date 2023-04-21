73f0bcb8be7584a937770b5d5921a277ceb0e843
IceDefcon <ice.defcon@yahoo.com>
2023-04-21 10:46:48 +0200

    Friendly fire code

--- BBB.Firmware/gpio/.hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/debian/bin/IceNET.NetworkAnalyser/BBB.Firmware/gpio/hello.ko := ld -r  -EL -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /home/debian/bin/IceNET.NetworkAnalyser/BBB.Firmware/gpio/hello.ko /home/debian/bin/IceNET.NetworkAnalyser/BBB.Firmware/gpio/hello.o /home/debian/bin/IceNET.NetworkAnalyser/BBB.Firmware/gpio/hello.mod.o ;  true
--- BBB.Firmware/gpio/.tmp_versions/hello.mod
+++
@@ -1,3 +0,0 @@
-/home/debian/bin/IceNET.NetworkAnalyser/BBB.Firmware/gpio/hello.ko
-/home/debian/bin/IceNET.NetworkAnalyser/BBB.Firmware/gpio/hello.o
-
