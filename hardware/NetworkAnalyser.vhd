1d4efa05498d2d32ff0653d069fc91e08ededd5f
IceDefcon <ice.defcon@yahoo.com>
2023-05-24 23:23:26 +0200

    Reverse changes in VHDL to make SPI work

--- hardware/NetworkAnalyser.vhd
+++ hardware/NetworkAnalyser.vhd
@@ -12,15 +12,15 @@ port
 	LED_1 			: out std_logic; 	-- PIN_7
 	LED_2 			: out std_logic;	-- PIN_9
 
-	S_CS 				: in 	std_logic; 	-- PIN_119 :: BBB P9_17 :: PULPLE 	:: SPI0_CS0
-	S_MOSI 			: in 	std_logic; 	-- PIN_121 :: BBB P9_18 :: BLUE 		:: SPI0_D1
-	S_MISO 			: out std_logic; 	-- PIN_125 :: BBB P9_21 :: BROWN 	:: SPI0_D0
-	S_SCLK 			: in 	std_logic; 	-- PIN_129 :: BBB P9_22 :: BLACK 	:: SPI0_SCLK
+	CS 				: in 	std_logic; 	-- PIN_119 :: BBB P9_17 :: PULPLE 	:: SPI0_CS0
+	MOSI 				: in 	std_logic; 	-- PIN_121 :: BBB P9_18 :: BLUE 		:: SPI0_D1
+	MISO 				: out std_logic; 	-- PIN_125 :: BBB P9_21 :: BROWN 	:: SPI0_D0
+	SCLK 				: in 	std_logic; 	-- PIN_129 :: BBB P9_22 :: BLACK 	:: SPI0_SCLK
 	
-	M_CS 				: out std_logic; 	-- PIN_118 :: BBB P9_28 :: YELOW 	:: SPI1_CS0
-	M_MOSI 			: out std_logic; 	-- PIN_120 :: BBB P9_30 :: GREEN 	:: SPI1_D1 :: GPIO_112
-	M_MISO 			: in 	std_logic; 	-- PIN_122 :: BBB P9_29 :: RED 		:: SPI1_D0
-	M_SCLK 			: out std_logic; 	-- PIN_126 :: BBB P9_31 :: ORANGE 	:: SPI1_SCLK
+	INT_CS 			: in 	std_logic; 	-- PIN_118 :: BBB P9_28 :: YELOW 	:: SPI1_CS0
+	INT_MOSI 		: in 	std_logic; 	-- PIN_120 :: BBB P9_30 :: GREEN 	:: SPI1_D1 :: GPIO_112
+	INT_MISO 		: out std_logic; 	-- PIN_122 :: BBB P9_29 :: RED 		:: SPI1_D0
+	INT_SCLK 		: in 	std_logic; 	-- PIN_126 :: BBB P9_31 :: ORANGE 	:: SPI1_SCLK
 	
 	BUTTON_IN 		: in std_logic; 	-- PIN_144
 	GPIO_IN 			: in std_logic; 	-- PIN_143
@@ -38,8 +38,6 @@ signal button_debounced	: std_logic := '1';
 signal inv_BUTTON_IN 	: std_logic := '1';
 
 signal clock_1Mhz 		: std_logic := '0';
-signal clock_25Mhz 		: std_logic := '0';
-signal clock_12_5Mhz 	: std_logic := '0';
 
 --------------------------------------------
 -- COMPONENTS DECLARATION
@@ -61,7 +59,7 @@ port
 	clk_out : out std_logic      -- 1 MHz output clock
 );
 end component;
- 
+	 
 --------------------------------------------
 -- MAIN ROUTINE
 --------------------------------------------
@@ -69,13 +67,8 @@ begin
 
 -- Combinational Logic
 inv_BUTTON_IN 	<= not BUTTON_IN; -- Buton is low active so must be inverted before debounce
-
--- Passing slave inputs to master outputs
---M_CS 	 <= S_CS;
---M_MOSI <= S_MOSI;
---S_MISO <= M_MISO;
---M_SCLK <= S_SCLK;
-S_MISO <= S_MOSI;
+MISO 				<= MOSI;
+INT_MISO 		<= INT_MOSI;
 
 debounce_module: debounce port map 
 (
@@ -95,26 +88,10 @@ gpio_process:
 process(CLOCK)
 begin
 	if rising_edge(CLOCK) then
-		LED_2 	<= '1'; --not BUTTON_IN; 	-- D5 Low Enable
-		LED_1 	<= '1'; --not GPIO_IN; 		-- D4 Low Enable
+		LED_2 	<= not BUTTON_IN; 			-- D5 Low Enable
+		LED_1 	<= not GPIO_IN; 				-- D4 Low Enable
 		LED_0 	<= not button_debounced; 	-- D2 Low Enable
 		GPIO_OUT <= button_debounced;
-	end if;
-end process;
-
-clock_25Mhz_process:
-process(CLOCK)
-begin
-	if rising_edge(CLOCK) then
-		clock_25Mhz <= not clock_25Mhz;
-	end if;
-end process;
-
-clock_12_5Mhz_process:
-process(clock_25Mhz)
-begin
-	if rising_edge(clock_25Mhz) then
-		clock_12_5Mhz <= not clock_12_5Mhz;
 	end if;
 end process;
 
