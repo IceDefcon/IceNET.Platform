5501b0300f227f89fa5e4162ac10d7093cb7c37e
IceDefcon <ice.defcon@yahoo.com>
2023-05-25 00:27:18 +0200

    Multiple devices of SPI type

--- firmware/include/devSpi.h
+++ firmware/include/devSpi.h
@@ -7,15 +7,22 @@
 #include "console.h"
 #include "devBase.h"
 
+#define MAX_SPI_DEVICES 2
+
 class DevSpi : public DevBase
 {
 private:
 	
-	int m_file_descriptor;
-    unsigned char m_rx_buffer[32];
-    uint8_t m_mode;
-    uint8_t m_bits_per_word;
-    uint32_t m_max_speed_hz;
+	typedef struct
+	{
+		int m_file_descriptor;
+	    unsigned char m_rx_buffer[32];
+	    uint8_t m_mode;
+	    uint8_t m_bits_per_word;
+	    uint32_t m_max_speed_hz;
+	} SpiConfig;
+
+	SpiConfig m_SpiConfig[MAX_SPI_DEVICES];
 
 public:
 	DevSpi();
