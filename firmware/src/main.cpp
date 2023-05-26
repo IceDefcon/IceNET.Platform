f55a494a8ddd6c5fe501666d95517c4f966511f8
IceDefcon <ice.defcon@yahoo.com>
2023-05-26 21:53:17 +0200

    Remove SPI from user space

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -7,12 +7,9 @@
 #include <unistd.h> // sleep
 
 #include "devChar.h"
-#include "devSpi.h"
 #include "console.h"
 
 #define ICE "/dev/iceCOM"
-#define SPI0 "/dev/spidev0.0"
-#define SPI1 "/dev/spidev1.0"
 
 void ConsoleThread() 
 {
@@ -35,26 +32,7 @@ int main(int argc, char* argv[])
 	pDevice->device_read();
 	pDevice->device_close();
 
-	// SPI 0
-	pDevice = pSpiDevice;
-	pDevice->device_open(SPI0);
-	// This must be dynamically casted to have access 
-	// to the non virtual methods out of the DevBase
-	dynamic_cast<DevSpi*>(pDevice)->device_setid();
-	int id = dynamic_cast<DevSpi*>(pDevice)->device_getid();
-	pDevice->device_write();
-	sleep(1);
-	pDevice->device_read();
-	pDevice->device_close();
-	// SPI 1
-	pDevice->device_open(SPI1);
-	pDevice->device_write();
-	sleep(1);
-	pDevice->device_read();
-	pDevice->device_close();
-	// Deallocate from heap
 	delete pCharDevice;
-	delete pSpiDevice;
 
 	return 0;
 }
