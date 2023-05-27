8b66dd640eaebba164196096508031187767039f
IceDefcon <ice.defcon@yahoo.com>
2023-05-27 02:10:28 +0200

    Testing Constructors

--- firmware/include/devBase.h
+++ firmware/include/devBase.h
@@ -8,6 +8,17 @@ class DevBase : public Console
 {
 public:
 
+	DevBase()
+	{
+		std::cout << "DevBase :: Construct" << std::endl;
+	}
+
+	~DevBase()
+	{
+		std::cout << "DevBase :: Destroy" << std::endl;
+
+	}
+	
 	virtual int device_open(const char* device) = 0;
 	virtual int device_write() = 0;
 	virtual int device_read() = 0;
