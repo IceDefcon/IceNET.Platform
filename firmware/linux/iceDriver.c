0803c2cefd7c350160e89cfcec4db1037295b97b
IceDefcon <ice.defcon@yahoo.com>
2023-05-30 11:34:24 +0200

    Set volatile buffers for SPI

--- firmware/linux/iceDriver.c
+++ firmware/linux/iceDriver.c
@@ -114,11 +114,11 @@ static int dev_release(struct inode *inodep, struct file *filep)
 static struct spi_device *spi_dev0;
 static struct spi_device *spi_dev1;
 
-static uint8_t tx_buffer0[] = {0xAA};  // Data to be transmitted for SPI0
-static uint8_t rx_buffer0[1];                            // Buffer to receive data for SPI0
+static volatile uint8_t tx_buffer0[] = {0xAA};  // Data to be transmitted for SPI0
+static volatile uint8_t rx_buffer0[1];          // Buffer to receive data for SPI0
 
-static uint8_t tx_buffer1[] = {0xBB};  // Data to be transmitted for SPI1
-static uint8_t rx_buffer1[1];                            // Buffer to receive data for SPI1
+static volatile uint8_t tx_buffer1[] = {0xBB};  // Data to be transmitted for SPI1
+static volatile uint8_t rx_buffer1[1];          // Buffer to receive data for SPI1
 
 static struct work_struct spi_work;
 static struct workqueue_struct *spi_wq;
