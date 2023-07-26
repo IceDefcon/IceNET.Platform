47db58d6f4bced24619238d3ea740675d7924109
IceDefcon <ice.defcon@yahoo.com>
2023-07-26 20:51:47 +0200

    Summer reminder xD

--- firmware/linux/iceDriver.c
+++ firmware/linux/iceDriver.c
@@ -97,11 +97,11 @@ static struct file_operations fops =
 static struct spi_device *spi_dev0;
 static struct spi_device *spi_dev1;
 
-static volatile uint8_t tx_res_buffer[] = {0xDCBA};  // Data to be transmitted for SPI0
-static volatile uint8_t rx_res_buffer[1];          // Buffer to receive data for SPI0
+static volatile uint8_t tx_res_buffer[] = {0xDCBA};     // Data to be transmitted for SPI0
+static volatile uint8_t rx_res_buffer[2];               // Buffer to receive data for SPI0
 
-static volatile uint8_t tx_req_buffer[] = {0xABCD};  // Data to be transmitted for SPI1
-static volatile uint8_t rx_req_buffer[1];          // Buffer to receive data for SPI1
+static volatile uint8_t tx_req_buffer[] = {0xABCD};     // Data to be transmitted for SPI1
+static volatile uint8_t rx_req_buffer[2];               // Buffer to receive data for SPI1
 
 //////////////////////////
 //                      //
