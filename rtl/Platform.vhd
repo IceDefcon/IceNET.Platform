b3eda1e5f42ee9796861192949bdb97c58186d02
IceDefcon <ice.defcon@yahoo.com>
2024-01-12 23:02:58 +0100

    State Machine :: Last glitch

--- rtl/Platform.vhd
+++ rtl/Platform.vhd
@@ -14,36 +14,36 @@ use ieee.std_logic_unsigned.all;
 entity Platform is
 port
 (
-	CLOCK_50MHz 	: in std_logic; 	-- PIN_T2
+    CLOCK_50MHz : in std_logic;     -- PIN_T2
 
-	-- For debuging
-	LED_1 			: out std_logic; 	-- PIN_U7
-	LED_2 			: out std_logic; 	-- PIN_U8
-	LED_3 			: out std_logic;	-- PIN_R7
-	LED_4 			: out std_logic; 	-- PIN_T8
-	LED_5 			: out std_logic; 	-- PIN_R8
-	LED_6 			: out std_logic;	-- PIN_P8
-	LED_7 			: out std_logic; 	-- PIN_M8
-	LED_8 			: out std_logic; 	-- PIN_N8
+    -- For debuging
+    LED_1 : out std_logic;    -- PIN_U7
+    LED_2 : out std_logic;    -- PIN_U8
+    LED_3 : out std_logic;    -- PIN_R7
+    LED_4 : out std_logic;    -- PIN_T8
+    LED_5 : out std_logic;    -- PIN_R8
+    LED_6 : out std_logic;    -- PIN_P8
+    LED_7 : out std_logic;    -- PIN_M8
+    LED_8 : out std_logic;    -- PIN_N8
 
-	KERNEL_CS 		: in  std_logic; 	-- PIN_A5 	:: BBB P9_17 :: PULPLE 	:: SPI0_CS0
-	KERNEL_MOSI 	: in  std_logic; 	-- PIN_A7 	:: BBB P9_18 :: BLUE 		:: SPI0_D1
-	KERNEL_MISO 	: out std_logic; 	-- PIN_A6 	:: BBB P9_21 :: BROWN 	:: SPI0_D0
-	KERNEL_SCLK 	: in  std_logic; 	-- PIN_A8 	:: BBB P9_22 :: BLACK 	:: SPI0_SCLK
+    KERNEL_CS : in std_logic;    -- PIN_A5   :: BBB P9_17 :: PULPLE  :: SPI0_CS0
+    KERNEL_MOSI : in std_logic;  -- PIN_A7   :: BBB P9_18 :: BLUE        :: SPI0_D1
+    KERNEL_MISO : out std_logic; -- PIN_A6   :: BBB P9_21 :: BROWN   :: SPI0_D0
+    KERNEL_SCLK : in std_logic;  -- PIN_A8   :: BBB P9_22 :: BLACK   :: SPI0_SCLK
 
-	I2C_IN_SDA 		: in 	std_logic; 	-- PIN_A9 	:: BBB P9_20 :: BLUE
-	I2C_IN_SCK 		: in 	std_logic; 	-- PIN_A10 	:: BBB P9_19 :: GREEN
+    I2C_IN_SDA : in std_logic;  -- PIN_A9   :: BBB P9_20 :: BLUE
+    I2C_IN_SCK : in std_logic;  -- PIN_A10  :: BBB P9_19 :: GREEN
 
-	I2C_OUT_SDA 	: out 	std_logic; 	-- PIN_B9 	::  		 :: RED
-	I2C_OUT_SCK 	: out 	std_logic; 	-- PIN_B10 	:: 			 :: ORANGE
+    I2C_OUT_SDA : out std_logic;  -- PIN_B9   ::           :: RED
+    I2C_OUT_SCK : out std_logic;  -- PIN_B10  ::           :: ORANGE
 
-	FPGA_INT		: out std_logic; 	-- PIN_A3 	:: BBB P9_12 :: BLACK
-	KERNEL_INT 		: in 	std_logic; 	-- PIN_A4 	:: BBB P9_14 :: WHITE
+    FPGA_INT : out std_logic;   -- PIN_A3   :: BBB P9_12 :: BLACK
+    KERNEL_INT : in std_logic;  -- PIN_A4   :: BBB P9_14 :: WHITE
 
-	BUTTON_1 		: in  std_logic; 	-- PIN_H20 	:: Reset
-	BUTTON_2 		: in  std_logic; 	-- PIN_K19 	:: Doesnt Work :: Broken Button or Incorrect Schematic
-	BUTTON_3 		: in  std_logic; 	-- PIN_J18
-	BUTTON_4 		: in  std_logic 	-- PIN_K18
+    BUTTON_1 : in std_logic;    -- PIN_H20  :: Reset
+    BUTTON_2 : in std_logic;    -- PIN_K19  :: Doesnt Work :: Broken Button or Incorrect Schematic
+    BUTTON_3 : in std_logic;    -- PIN_J18
+    BUTTON_4 : in std_logic     -- PIN_K18
 );
 end Platform;
 
@@ -69,22 +69,19 @@ signal synced_miso  			: std_logic := '0';
 
 -- Reset Button
 signal reset_button			: std_logic := '0';
-signal idle_button			: std_logic := '0';
 
 -- i2c clocks
-signal i2c_half_wave : std_logic_vector(7 downto 0) := (others => '0');
-signal i2c_clock_d0 : std_logic := '0';
-signal i2c_clock_d1 : std_logic := '0';
-signal i2c_clock_d2 : std_logic := '0';
-signal i2c_clock_d3 : std_logic := '0';
+signal i2c_clock : std_logic := '0';
+signal i2c_clock_delay : std_logic_vector(7 downto 0) := (others => '0');
 signal i2c_clock_align : std_logic := '0';
+signal i2c_clock_first : std_logic := '0';
 
 -- i2c initialise time
 signal init_delay : std_logic_vector(26 downto 0) := (others => '0');
 signal device_delay : std_logic_vector(26 downto 0) := (others => '0');
 
 --i2c state machine
-type TX is (RESET, INIT, DEVICE);
+type TX is (RESET, INIT, DEVICE, BARIER, REG);
 signal tx_current_state, tx_next_state: TX := RESET;
 
 -- i2c state machine flags
@@ -93,7 +90,7 @@ signal isINIT : std_logic := '0';
 signal isDEVICE : std_logic := '0';
 
 -- i2c write clock
-signal write_clock : std_logic := '0';
+signal write_clock : std_logic := '1';
 signal write_detect : std_logic := '0';
 signal write_count : std_logic_vector(3 downto 0) := "0000";
 
@@ -185,7 +182,7 @@ Debounce_module: Debounce port map
 	button_in_4 => BUTTON_4,
 	button_out_1 => reset_button,
 	button_out_2 => open,
-	button_out_3 => idle_button,
+	button_out_3 => open,
 	button_out_4 => open
 );
 
@@ -254,25 +251,18 @@ clock_free_process:
 process(CLOCK_50MHz)
 begin
     if rising_edge(CLOCK_50MHz) then
-        if i2c_half_wave = "11111001" then
-            i2c_clock_d0 <= not i2c_clock_d0;
-            i2c_half_wave <= (others => '0'); -- Reset count
+        if i2c_clock_delay = "11111001" then
+            i2c_clock <= not i2c_clock;
+            i2c_clock_delay <= (others => '0'); -- Reset count
         else
-            i2c_half_wave <= i2c_half_wave + '1';
+            i2c_clock_delay <= i2c_clock_delay + '1';
         end if;
-
-        -- Assign delayed clocks unconditionally on every clock edge
-        i2c_clock_d1 <= i2c_clock_d0;
-        i2c_clock_d2 <= i2c_clock_d1;
-        i2c_clock_d3 <= i2c_clock_d2;
     end if;
 end process;
 
 state_machine_process:
-process(CLOCK_50MHz, reset_button, sm_delay, init_delay, device_delay, sm_run,
-	i2c_clock_d0, i2c_clock_d1, i2c_clock_d2, i2c_clock_d3, i2c_clock_align,
-	tx_current_state, tx_next_state, isRESET, isINIT, isDEVICE,
-	write_clock, write_detect, write_count)
+process(CLOCK_50MHz, reset_button, sm_delay, init_delay, device_delay, sm_run, i2c_clock, i2c_clock_align,
+	tx_current_state, tx_next_state, isRESET, isINIT, isDEVICE,	write_clock, write_detect, write_count)
 begin
     if rising_edge(CLOCK_50MHz) then
 
@@ -285,7 +275,7 @@ begin
 				isRESET <= '0';
 				isINIT <= '0';
 				isDEVICE <= '0';
-		        LED_1 <= '0'; -- Debuging LED's
+		      LED_1 <= '0'; -- Debuging LED's
 				LED_2 <= '0';
 				LED_3 <= '0';
 				LED_4 <= '0';
@@ -325,35 +315,38 @@ begin
 	        -- State Machine :: DEVICE Process
 	        ----------------------------------------
 	        if tx_current_state = DEVICE then
-			    if i2c_clock_d1 = '0' then 	-- Align @ 1 to achieve cycle from '0'
-			        i2c_clock_align <= '1';
-			    else
-			        i2c_clock_align <= '0';
+			    if i2c_clock = '1' then 	-- Align @ 1 to achieve cycle from '0'
+			        i2c_clock_align <= '1'; -- Stay HI until reset
 			    end if;
 
 			    -- Clock is aligned
 			    if i2c_clock_align = '1' then
-			        if i2c_clock_d0 = '0' then  -- First '0' after aligned '1'
+			        if i2c_clock = '0' then  -- First '0' after aligned '1'
 
-			        	write_clock <= i2c_clock_d0;
+				    	i2c_clock_first <= '1';
 
 			            -- End of DEVICE address transmission
 			            if write_count = "1111" then
 			                write_count <= "0000";
-			                write_clock <= '0';
 			            	tx_next_state <= RESET;
 			                isDEVICE <= '1';
-			                sm_run <= '0'; 			-- Stop State Machine
+
 			                i2c_clock_align <= '0'; -- Reset Alignment
+				    		i2c_clock_first <= '0'; -- Reset clock first
+				    		write_detect <= '0'; 	-- Reset clock detection
+							write_clock <= '1'; 	-- Pull up after TX is complete
+
+			                sm_run <= '0'; 			-- Hold State Machine
 			            end if;
 
 			            -- Write clock cycle is detected
 						if write_detect = '0' then
 			                write_count <= write_count + '1';
 							write_detect <= '1';
+							write_clock <= '0'; 				-----===[ OUT LO ]===-----
 						end if;
-					else 
-						write_clock	<= i2c_clock_d0;
+					elsif i2c_clock_first = '1' then
+						write_clock <= '1'; 					-----===[ OUT HI ]===-----
 						write_detect <= '0';
 			        end if;
 			    end if;
@@ -377,11 +370,13 @@ end process;
 
 
 i2c_process:
-process(CLOCK_50MHz, sda, sck)
+process(CLOCK_50MHz, write_clock, sda, sck)
 begin
 	if rising_edge(CLOCK_50MHz) then
 		I2C_OUT_SCK <= write_clock;
 		I2C_OUT_SDA <= '0';
+		--sck <= I2C_IN_SCK;
+		--sda <= I2C_IN_SDA;
 	end if;
 end process;
 
