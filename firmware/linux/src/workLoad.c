521d6b0be15816d722e02a9d70547dd87e0566f2
IceDefcon <ice.defcon@yahoo.com>
2024-03-21 00:30:17 +0100

    Connecting char device + work load

--- firmware/linux/src/workLoad.c
+++ firmware/linux/src/workLoad.c
@@ -23,13 +23,45 @@ static struct workqueue_struct* ice_fpga_wq;
 static struct workqueue_struct* ice_kernel_wq;
 
 /* GET */
-struct workqueue_struct* get_fpga_wq(void) return ice_fpga_wq;
-struct workqueue_struct* get_kernel_wq(void) return ice_kernel_wq;
-struct work_struct* get_fpga_work(void) return &ice_fpga_work;
-struct work_struct* get_kernel_work(void) return &ice_kernel_work;
+struct workqueue_struct* get_fpga_wq(void)
+{
+	return ice_fpga_wq;
+}
+
+struct workqueue_struct* get_kernel_wq(void)
+{
+	return ice_kernel_wq;
+}
+
+struct work_struct* get_fpga_work(void)
+{
+	return &ice_fpga_work;
+}
+
+struct work_struct* get_kernel_work(void)
+{
+	return &ice_kernel_work;
+}
+
 
 /* SET */
-void set_fpga_wq(struct workqueue_struct *wq) ice_fpga_wq = wq;
-void set_kernel_wq(struct workqueue_struct *wq) ice_kernel_wq = wq;
-void set_fpga_work(struct work_struct *work) ice_fpga_work = *work;
-void set_kernel_work(struct work_struct *work) ice_kernel_work = *work;
+void set_fpga_wq(struct workqueue_struct *wq) 
+{
+	ice_fpga_wq = wq;
+}
+
+void set_kernel_wq(struct workqueue_struct *wq) 
+{
+	ice_kernel_wq = wq;
+}
+
+void set_fpga_work(struct work_struct *work) 
+{
+	ice_fpga_work = *work;
+}
+
+void set_kernel_work(struct work_struct *work) 
+{
+	ice_kernel_work = *work;
+}
+
