a278d9e17925c60af01456e8c8fbb4a1e734d90d
IceDefcon <ice.defcon@yahoo.com>
2024-03-20 23:39:50 +0100

    Split module

--- firmware/linux/src/charDevice.c
+++ firmware/linux/src/charDevice.c
@@ -44,8 +44,7 @@ ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *of
 
     if(strncmp(message, "a", 1) == 0)
     {
-        tx_fpga[0] = 0x0F;
-        queue_work(fpga_wq, &fpga_work);
+        // queue_work(fpga_wq, &fpga_work);
     }
 
     if (error_count==0)
