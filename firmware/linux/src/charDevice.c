dad31ec4b7db1ae86dbbd1809bece024aa07dde6
IceDefcon <ice.defcon@yahoo.com>
2024-03-21 00:56:14 +0100

    Connecting char device + work load

--- firmware/linux/src/charDevice.c
+++ firmware/linux/src/charDevice.c
@@ -51,8 +51,6 @@ ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *of
     int error_count = 0;
     error_count = copy_from_user(message, buffer, len);
 
-    struct work_struct temp_fpga_work;
-    s
     if(strncmp(message, "a", 1) == 0)
     {
         queue_work(get_fpga_wq(), get_fpga_work());
