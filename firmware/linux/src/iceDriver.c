f36d290d22dc196f7a0af78c9e362a6e3fe27c4b
IceDefcon <ice.defcon@yahoo.com>
2024-03-21 01:14:35 +0100

    Connecting char device + work load

--- firmware/linux/src/iceDriver.c
+++ firmware/linux/src/iceDriver.c
@@ -48,9 +48,9 @@ MODULE_DESCRIPTION("FPGA Comms Driver");
 //                  //
 //////////////////////
 // static struct work_struct fpga_work;
-static struct work_struct kernel_work;
+// static struct work_struct kernel_work;
 // static struct workqueue_struct *fpga_wq;
-static struct workqueue_struct *kernel_wq;
+// static struct workqueue_struct *kernel_wq;
 
 //////////////////////
 //                  //
@@ -428,7 +428,7 @@ static irqreturn_t isr_kernel(int irq, void *data)
     printk(KERN_INFO "[FPGA][ISR] Kernel interrupt [%d] @ Pin [%d]\n", counter, GPIO_KERNEL_INTERRUPT);
     counter++;
 
-    queue_work(kernel_wq, &kernel_work);
+    queue_work(get_kernel_wq(), get_kernel_work());
 
     return IRQ_HANDLED;
 }
@@ -503,9 +503,11 @@ static int __init fpga_driver_init(void)
      * SPI operations
      * Kernel and Fpga
      */
-    INIT_WORK(&kernel_work, kernel_execute);
-    kernel_wq = create_singlethread_workqueue("kernel_workqueue");
-    if (!kernel_wq) {
+    struct work_struct tmp_kernel_work;
+    INIT_WORK(&tmp_kernel_work, kernel_execute);
+    set_kernel_work(&tmp_kernel_work)
+    set_kernel_wq(create_singlethread_workqueue("kernel_workqueue"));
+    if (!get_kernel_wq()) {
         printk(KERN_ERR "[FPGA][WRK] Failed to create kernel workqueue\n");
         return -ENOMEM;
     }
@@ -513,7 +515,7 @@ static int __init fpga_driver_init(void)
     struct work_struct tmp_fpga_work;
     INIT_WORK(&tmp_fpga_work, fpga_command);
     set_fpga_work(&tmp_fpga_work);
-    
+
     set_fpga_wq(create_singlethread_workqueue("fpga_workqueue"));
     if (!get_fpga_wq()) {
         printk(KERN_ERR "[FPGA][WRK] Failed to create fpga workqueue\n");
@@ -616,11 +618,11 @@ static void __exit fpga_driver_exit(void)
     // SPI :: CONFIG                //
     //                              //
     //////////////////////////////////
-    cancel_work_sync(&kernel_work);
-    if (kernel_wq) {
-        flush_workqueue(kernel_wq);
-        destroy_workqueue(kernel_wq);
-        kernel_wq = NULL;
+    cancel_work_sync(get_kernel_work());
+    if (get_kernel_wq()) {
+        flush_workqueue(get_kernel_wq());
+        destroy_workqueue(get_kernel_wq());
+        set_kernel_wq(NULL);
     }
 
     cancel_work_sync(get_fpga_work());
