485811b4b08d19c82227208a070a80b67f35a8a1
IceDefcon <ice.defcon@yahoo.com>
2024-03-25 00:52:42 +0100

    Fix workload

--- firmware/linux/src/iceDriver.c
+++ firmware/linux/src/iceDriver.c
@@ -30,6 +30,19 @@ MODULE_DESCRIPTION("FPGA Comms Driver");
 //                  //
 //                  //
 //                  //
+//   [W] Workload   //
+//                  //
+//                  //
+//                  //
+//////////////////////
+
+static struct work_struct kernel_work;
+static struct workqueue_struct *kernel_wq;
+
+//////////////////////
+//                  //
+//                  //
+//                  //
 //    [C] Device    //
 //                  //
 //                  //
@@ -171,7 +184,7 @@ int StateMachineThread(void *data)
                 break;
 
             case USER:
-                printk(KERN_INFO "[FPGA][STM] IDLE State [%d]\n",counter);
+                printk(KERN_INFO "[FPGA][STM] USER State [%d]\n",counter);
                 STATE = IDLE;
                 break;
 
@@ -207,6 +220,50 @@ static int dev_open(struct inode *inodep, struct file *filep)
     numberOpens++;
     printk(KERN_INFO "[FPGA][ C ] Device has been opened %d time(s)\n", numberOpens);
     return NULL;
+}
+
+static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset)
+{
+    int error_count = 0;
+    //
+    // Copy to user space :: *to, *from, size :: returns 0 on success
+    //
+    error_count = copy_to_user(buffer, message, size_of_message);
+    memset(message, 0, sizeof(message));
+
+    if (error_count==0)
+    {
+        printk(KERN_INFO "[FPGA][ C ] Sent %d characters to the user\n", size_of_message);
+        return (size_of_message = 0);  // clear the position to the start and return NULL
+    }
+    else 
+    {
+        printk(KERN_INFO "[FPGA][ C ] Failed to send %d characters to the user\n", error_count);
+        return -EFAULT; // Failed -- return a bad address message (i.e. -14)
+    }
+}
+
+static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset)
+{
+    int error_count = 0;
+    error_count = copy_from_user(message, buffer, len);
+
+    if(strncmp(message, "a", 1) == 0)
+    {
+        queue_work(get_fpga_wq(), get_fpga_work());
+    }
+
+    if (error_count==0)
+    {
+        size_of_message = strlen(message);
+        printk(KERN_INFO "[FPGA][ C ] Received %d characters from the user\n", len);
+        return len;
+    } 
+    else 
+    {
+        printk(KERN_INFO "[FPGA][ C ] Failed to receive characters from the user\n");
+        return -EFAULT;
+    }
 }
 
 static int dev_release(struct inode *inodep, struct file *filep)
