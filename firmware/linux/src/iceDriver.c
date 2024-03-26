eca3d2823809530d417fac0aaaf7dc6656d8b09a
IceDefcon <ice.defcon@yahoo.com>
2024-03-26 07:54:49 +0100

    Split char device

--- firmware/linux/src/iceDriver.c
+++ firmware/linux/src/iceDriver.c
@@ -47,8 +47,8 @@ static struct device* C_Device = NULL;
 
 static int     dev_open(struct inode *, struct file *);
 static int     dev_release(struct inode *, struct file *);
-static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
-static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
+// static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
+// static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
 
 static DEFINE_MUTEX(com_mutex);
 
