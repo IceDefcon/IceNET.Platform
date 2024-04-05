07f12a61bcc317df619453d3e3349b5e2cd97aef
IceDefcon <ice.defcon@yahoo.com>
2024-04-05 21:50:08 +0200

    State Machine module

---
+++ rtl/temp.vhd
@@ -0,0 +1,22 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity temp is
+port
+(    
+    clock : in std_logic;
+    reset : in std_logic;
+
+    input : in std_logic;
+    output : out std_logic
+);
+end temp;
+
+architecture rtl of temp is
+
+--
+-- CODE
+--
+
+end rtl;
