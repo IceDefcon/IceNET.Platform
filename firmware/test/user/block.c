8a893b8b7c4282bb16c5eda98635971ea3a86444
IceDefcon <ice.defcon@yahoo.com>
2024-04-21 17:13:18 +0200

    Open block device from user space

--- firmware/test/user/block.c
+++ firmware/test/user/block.c
@@ -14,22 +14,6 @@ int main() {
         return 1;
     }
 
-    // Read data from the block device
-    if (read(fd, buffer, sizeof(buffer)) == -1) {
-        perror("Failed to read from block device");
-        close(fd);
-        return 1;
-    }
-
-    // Write data to the block device
-    // Example: Write "Hello" to the first block
-    char *data = "Hello";
-    if (write(fd, data, strlen(data)) == -1) {
-        perror("Failed to write to block device");
-        close(fd);
-        return 1;
-    }
-
     // Close the block device
     close(fd);
 
