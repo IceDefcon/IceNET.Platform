b03e12095bf27e850c64d3d1926fd2887accb78b
IceDefcon <ice.defcon@yahoo.com>
2024-04-21 23:57:31 +0200

    Debugubg registration of block device

--- firmware/test/iceBlock.c
+++ firmware/test/iceBlock.c
@@ -8,30 +8,39 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Ice Marek");
 MODULE_DESCRIPTION("Block Device Object");
 
-
-
-
 #define MY_BLOCK_MAJOR           240
 #define MY_BLKDEV_NAME          "mybdev"
+#define MY_BLOCK_MINORS       1
+
+static struct my_block_dev 
+{
+    struct gendisk *gd;
+} dev;
+
+static int create_block_device(struct my_block_dev *dev)
+{
+    dev->gd = alloc_disk(MY_BLOCK_MINORS);
+    add_disk(dev->gd);
+}
 
 static int my_block_init(void)
 {
     int status;
 
     status = register_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME);
-    if (status < 0) {
-             printk(KERN_ERR "unable to register mybdev block device\n");
-             return -EBUSY;
-     }
-     //...
+    if (status < 0) 
+    {
+        printk(KERN_ERR "unable to register mybdev block device\n");
+        return -EBUSY;
+    }
+
+    create_block_device(&dev);
 }
 
 static void my_block_exit(void)
 {
-     //...
-     unregister_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME);
+    unregister_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME);
 }
-
 
 module_init(my_block_init);
 module_exit(my_block_exit);
