2218b40f09d7c11e4732ba1819359454ccb0613c
IceDefcon <ice.defcon@yahoo.com>
2024-04-22 20:04:37 +0200

    SPI1 ready for FPGA comms

--- firmware/linux/include/spiWork.h
+++ firmware/linux/include/spiWork.h
@@ -20,10 +20,10 @@
 //                  //
 //////////////////////
 
-/* GET WORK QUEUE*/ struct workqueue_struct* get_testFromCharDevice_wq(void) 
+/* GET WORK QUEUE*/ struct workqueue_struct* get_testFromCharDevice_wq(void);
 /* GET WORK QUEUE */ struct workqueue_struct* get_signalFromCharDevice_wq(void);
 /* GET WORK QUEUE */ struct workqueue_struct* get_interruptFromFpga_wq(void);
-/* GET WORK */ struct work_struct* get_testFromCharDevice_work(void) 
+/* GET WORK */ struct work_struct* get_testFromCharDevice_work(void);
 /* GET WORK */ struct work_struct* get_signalFromCharDevice_work(void);
 /* GET WORK */ struct work_struct* get_interruptFromFpga_work(void);
 
