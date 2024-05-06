2402ef764d0754f9fed303a6d7e7e35270f0ae14
IceDefcon <ice.defcon@yahoo.com>
2024-05-06 20:22:27 +0200

    console rework

--- firmware/app/src/iceCOM.cpp
+++ firmware/app/src/iceCOM.cpp
@@ -128,8 +128,6 @@ int iceCOM::device_read()
     }
 }
 
-#include <iostream>
-#include <iomanip>
 #include <sstream>
 
 int iceCOM::device_write()
@@ -140,33 +138,16 @@ int iceCOM::device_write()
     /* Get console characters */
     std::cin.getline(consoleControl.data(), CONSOLE_CONTROL_SIZE);
 
-    std::cout << "Character[0] " << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(consoleControl[0]) << std::endl;
-    std::cout << "Character[1] " << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(consoleControl[1]) << std::endl;
-    std::cout << "Character[2] " << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(consoleControl[2]) << std::endl;
-    std::cout << "Character[3] " << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(consoleControl[3]) << std::endl;
-    std::cout << consoleControl[0] << std::endl;
-    std::cout << consoleControl[1] << std::endl;
-    std::cout << consoleControl[2] << std::endl;
-    std::cout << consoleControl[3] << std::endl;
-
-
-
-
-    char oneByte;
-
-    // Convert ASCII character to hexadecimal string
+    // Convert the input to a hexadecimal value
+    std::string hexString(consoleControl.begin(), consoleControl.end());
+    int hexValue;
     std::stringstream ss;
-    ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(consoleControl[3]);
-    std::string hexStr = ss.str();
+    ss << std::hex << hexString;
+    ss >> hexValue;
 
-    // Extract the first character from the hexadecimal string and assign it to oneByte
-    if (!hexStr.empty()) {
-        oneByte = hexStr[0];
-    }
-
-    std::cout << "Final: " << oneByte;
-
-
+    // Print the hexadecimal value
+    std::cout << "Hex value: 0x" << std::hex << hexValue << std::endl;
+    
     if (std::strcmp(consoleControl.data(), "exit") == 0) 
     {
         m_killThread = true;
