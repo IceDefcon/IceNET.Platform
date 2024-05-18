cbfe34b6a1c65c20c1fc4fe9944bbec9f2b72d39
IceDefcon <ice.defcon@yahoo.com>
2024-05-19 00:49:33 +0200

    Offload bytes ready

--- rtl/Platform.vhd
+++ rtl/Platform.vhd
@@ -98,6 +98,7 @@ signal kernel_interrupt : std_logic := '0';
 signal kernel_interrupt_stop : std_logic := '0';
 -- Offload
 signal offload_interrupt : std_logic := '0';
+signal offload_reset : std_logic := '0';
 signal offload_ready : std_logic := '0';
 signal offload_ctrl : std_logic_vector(7 downto 0) := (others => '0');
 signal offload_data : std_logic_vector(7 downto 0) := (others => '0');
@@ -107,11 +108,7 @@ type STATE is
     IDLE,
     SPIN
 );
-signal state_current, state_next: STATE := IDLE;
--- Test
-signal test_1 : std_logic := '0';
-signal test_2 : std_logic := '0';
-signal test_3 : std_logic := '0';
+signal offload_state: STATE := IDLE;
 
 ----------------------------------------------------------------------------------------------------------------
 -- COMPONENTS DECLARATION
@@ -390,87 +387,46 @@ port map
     offload  => primary_fifo_offload
 );
 
-
-offload_process: process(CLOCK_50MHz)
+process (CLOCK_50MHz)
 begin
     if rising_edge(CLOCK_50MHz) then
+        case offload_state is
 
-        ------------------------------------
-        -- State Machine :: IDLE
-        ------------------------------------
-        if state_current = IDLE then
-            offload_ready <= '0';
-            if offload_interrupt = '1' then
-                state_next <= SPIN;
-            end if;
-            LED_6 <= '1';
-            LED_7 <= '0';
-        end if;
+            when IDLE =>
+                offload_ready <= '0';
+                if offload_interrupt = '1' then
+                    offload_state <= SPIN;
+                else
+                    offload_state <= IDLE;
+                end if;
 
-        ------------------------------------
-        -- State Machine :: SPIN
-        ------------------------------------
-        if state_current = SPIN then
-            if offload_count < "10" then
-                primary_fifo_rd_en <= '1';
-                offload_count <= offload_count + '1';
-            elsif offload_count = "10" then
-                primary_fifo_rd_en <= '0';
-                offload_ready <= '1';
-                offload_count <= "00";
-                state_next <= IDLE;
-            end if;
-            LED_6 <= '0';
-            LED_7 <= '1';
-        end if;
+            when SPIN =>
+                if offload_count = "00" then
+                    primary_fifo_rd_en <= '1';
+                    offload_count <= offload_count + '1';
+                    offload_state <= SPIN;
+                elsif offload_count = "01" then
+                    primary_fifo_rd_en <= '1';
+                    offload_count <= offload_count + '1';
+                    offload_state <= SPIN;
+                elsif offload_count = "10" then
+                    primary_fifo_rd_en <= '0';
+                    offload_count <= offload_count + '1';
+                    offload_data <= primary_fifo_data_out; -- Read data.byte
+                    offload_state <= SPIN;
+                else
+                    offload_ctrl <= primary_fifo_data_out; -- Read ctrl.byte
+                    offload_ready <= '1';
+                    offload_count <= "00";
+                    offload_state <= IDLE;
+                end if;
 
-        ------------------------------------
-        -- State Machine :: Update
-        ------------------------------------
-        state_current <= state_next;
+            when others =>
+                offload_state <= IDLE;
 
-        ------------------------------------
-        -- Offload
-        ------------------------------------
-        if offload_count = "01" then
-            offload_data <= primary_fifo_data_out;
-        elsif offload_count = "10" then
-            offload_ctrl <= primary_fifo_data_out;
-        end if;
-
+        end case;
     end if;
 end process;
-
---
--- TEST
---
---process_test:
---process(CLOCK_50MHz)
---begin
---    if rising_edge(CLOCK_50MHz) then
---        if test_1 = '0' then
---            test_1 <= '1';
---        end if;
-
---        if test_1 = '1' then
---            test_2 <= '1';
---        end if;
-        
---        if test_2 = '1' then
---            test_3 <= '1';
---        end if;
-        
---        if test_3 = '1' then
---            test_1 <= '0';
---            test_2 <= '0';
---            test_3 <= '0';
---        end if;
---    end if;
---end process;
-
---LED_6 <= offload_data(0) or offload_data(1) or offload_data(2) or offload_data(3);
---LED_7 <= offload_data(4) or offload_data(5) or offload_data(6) or offload_data(7) or offload_ready;
-LED_8 <= offload_ctrl(0) or offload_ctrl(1) or offload_ctrl(2) or offload_ctrl(3) or offload_ctrl(4) or offload_ctrl(5) or offload_ctrl(6) or offload_ctrl(7) or test_1 or test_2 or test_3;
 
 -----------------------------------------------
 -- Interrupt is pulled down
--- rtl/temp.vhd
+++ rtl/temp.vhd
@@ -15,8 +15,55 @@ end temp;
 
 architecture rtl of temp is
 
---
--- CODE
---
+type STATE is 
+(
+    IDLE,
+    SPIN,
+    DONE
+);
+signal state_current, state_next: STATE := IDLE;
+-- Test
+signal isIDLE : std_logic := '0';
+signal isSPIN : std_logic := '0';
+signal isDONE : std_logic := '0';
+signal state_current, state_next: STATE := IDLE;
+
+offload_process:
+process (CLOCK_50MHz, offload_reset)
+begin
+    if offload_reset = '1' then
+        state_current <= IDLE;
+    elsif rising_edge(CLOCK_50MHz) then
+        case state_current is
+            when IDLE =>
+                isIDLE <= '1';
+                isSPIN <= '0';
+                isDONE <= '0';
+                if offload_interrupt = '1' then
+                    state_next <= SPIN;
+                else
+                    state_next <= IDLE;
+                end if;
+
+            when SPIN =>
+                isIDLE <= '0';
+                isSPIN <= '1';
+                isDONE <= '0';
+                state_next <= DONE;
+
+            when DONE =>
+                isIDLE <= '0';
+                isSPIN <= '0';
+                isDONE <= '1';
+                state_next <= IDLE;
+
+            when others =>
+                state_next <= IDLE;
+
+        end case;
+
+        state_current <= state_next;
+    end if;
+end process;
 
 end rtl;
