228371aee481285b7a23eac5a387908e98fa6ca5
IceDefcon <ice.defcon@yahoo.com>
2024-06-07 23:43:51 +0200

    Link kernel-space iceNET with user-space

--- firmware/linux/src/charDevice.c
+++ firmware/linux/src/charDevice.c
@@ -23,20 +23,25 @@
 //                  //
 //                  //
 //                  //
-//   [ C ] Device   //
+//   [ x ] Device   //
 //                  //
 //                  //
 //                  //
 //////////////////////
 
-#define  DEVICE_NAME "iceCOM"
-#define  CLASS_NAME  "iceCOM"
+#define  DEVICE_COM "iceCOM"
+#define  CLASS_COM  "iceCOM"
 
-static int majorNumber;
-static struct class*  C_Class  = NULL;
-static struct device* C_Device = NULL;
-static int numberOpens = 0;
+#define  DEVICE_NET "iceNET"
+#define  CLASS_NET  "iceNET"
+
+static int majorNumber[2];
+static struct class*  C_Class[2]  = {NULL,NULL};
+static struct device* C_Device[2] = {NULL,NULL};
+static int numberOpens[2] = {0,0};
+
 static DEFINE_MUTEX(com_mutex);
+static DEFINE_MUTEX(net_mutex);
 
 static int dev_open(struct inode *inodep, struct file *filep);
 static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
@@ -45,12 +50,26 @@ static int dev_release(struct inode *inodep, struct file *filep);
 
 static DataTransfer charDeviceTransfer; 
 
-static struct file_operations fops =
+enum deviceTYPE
 {
-   .open = dev_open,
-   .read = dev_read,
-   .write = dev_write,
-   .release = dev_release,
+    ICE_COM,
+    ICE_NET
+};
+
+static struct file_operations fops[2] =
+{
+   {
+       .open = dev_open,
+       .read = dev_read,
+       .write = dev_write,
+       .release = dev_release,
+   },
+   {
+       .open = dev_open,
+       .read = dev_read,
+       .write = dev_write,
+       .release = dev_release,
+   }
 };
 
 static void init_charDevice_Data(void)
@@ -61,14 +80,14 @@ static void init_charDevice_Data(void)
     RxData = kmalloc(1 + 1, GFP_KERNEL); /* Register + Null terminate */
     if (!RxData) 
     {
-        printk(KERN_ALERT "[CTRL][ C ] RxData :: Memory allocation failed ");
+        printk(KERN_ALERT "[CTRL][COM] RxData :: Memory allocation failed ");
     }
 
     /* Allocate memory for TxData */
     TxData = kmalloc(1 + 1, GFP_KERNEL); /* Register + null terminate */
     if (!TxData) 
     {
-        printk(KERN_ALERT "[CTRL][ C ] TxData :: Memory allocation failed ");
+        printk(KERN_ALERT "[CTRL][COM] TxData :: Memory allocation failed ");
     }
 
     TxData[0] = 0xBB; /* C Device Preamble */
@@ -79,7 +98,7 @@ static void init_charDevice_Data(void)
     charDeviceTransfer.length = 2;
     charDeviceTransfer.ready = false;
 
-    printk(KERN_ALERT "[INIT][ C ] Initialize charDevice Data\n");
+    printk(KERN_ALERT "[INIT][COM] Initialize charDevice Data\n");
 }
 
 /* GET TRANSFER RX DATA */ DataTransfer* charDevice_getRxData(void) 
@@ -89,99 +108,198 @@ static void init_charDevice_Data(void)
 
 void charDeviceInit(void)
 {
-    majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
-    if (majorNumber<0)
+    //
+    // iceCOM
+    //
+    majorNumber[ICE_COM] = register_chrdev(0, DEVICE_COM, &fops[ICE_COM]);
+    if (majorNumber[ICE_COM]<0)
     {
-        printk(KERN_ALERT "[INIT][ C ] Failed to register major number: %d\n", majorNumber);
+        printk(KERN_ALERT "[INIT][COM] Failed to register major number: %d\n", majorNumber[ICE_COM]);
     }
     else
     {
-        printk(KERN_ALERT "[INIT][ C ] Register major number for char Device: %d\n", majorNumber);
+        printk(KERN_ALERT "[INIT][COM] Register major number for char Device: %d\n", majorNumber[ICE_COM]);
     }
 
-    C_Class = class_create(THIS_MODULE, CLASS_NAME);
-    if (IS_ERR(C_Class))
+    C_Class[ICE_COM] = class_create(THIS_MODULE, CLASS_COM);
+    if (IS_ERR(C_Class[ICE_COM]))
     {
-        unregister_chrdev(majorNumber, DEVICE_NAME);
-        printk(KERN_ALERT "[INIT][ C ] Failed to register device class: %ld\n", PTR_ERR(C_Class));
+        unregister_chrdev(majorNumber[ICE_COM], DEVICE_COM);
+        printk(KERN_ALERT "[INIT][COM] Failed to register device class: %ld\n", PTR_ERR(C_Class[ICE_COM]));
     }
     else
     {
-        printk(KERN_ALERT "[INIT][ C ] Register device class\n");
+        printk(KERN_ALERT "[INIT][COM] Register device class\n");
     }
     
-    C_Device = device_create(C_Class, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
-    if (IS_ERR(C_Device))
+    C_Device[ICE_COM] = device_create(C_Class[ICE_COM], NULL, MKDEV(majorNumber[ICE_COM], 0), NULL, DEVICE_COM);
+    if (IS_ERR(C_Device[ICE_COM]))
     {
-        class_destroy(C_Class);
-        unregister_chrdev(majorNumber, DEVICE_NAME);
-        printk(KERN_ALERT "[INIT][ C ] Failed to create the device\n");
+        class_destroy(C_Class[ICE_COM]);
+        unregister_chrdev(majorNumber[ICE_COM], DEVICE_COM);
+        printk(KERN_ALERT "[INIT][COM] Failed to create the device\n");
     }
     else
     {
-        printk(KERN_ALERT "[INIT][ C ] Create char Device\n");
+        printk(KERN_ALERT "[INIT][COM] Create char Device\n");
     }
 
     init_charDevice_Data();
 
-    printk(KERN_ALERT "[INIT][ C ] Lock on [C] Device Mutex\n");
+    printk(KERN_ALERT "[INIT][COM] Lock on [C] Device Mutex\n");
     mutex_init(&com_mutex);
+
+    //
+    // iceNET
+    //
+    majorNumber[ICE_NET] = register_chrdev(0, DEVICE_NET, &fops[ICE_NET]);
+    if (majorNumber[ICE_NET] < 0)
+    {
+        printk(KERN_ALERT "[INIT][NET] Failed to register major number: %d\n", majorNumber[ICE_NET]);
+    }
+    else
+    {
+        printk(KERN_ALERT "[INIT][NET] Register major number for char Device: %d\n", majorNumber[ICE_NET]);
+    }
+
+    C_Class[ICE_NET] = class_create(THIS_MODULE, CLASS_NET);
+    if (IS_ERR(C_Class[ICE_NET]))
+    {
+        unregister_chrdev(majorNumber[ICE_NET], DEVICE_NET);
+        printk(KERN_ALERT "[INIT][NET] Failed to register device class: %ld\n", PTR_ERR(C_Class[ICE_NET]));
+    }
+    else
+    {
+        printk(KERN_ALERT "[INIT][NET] Register device class\n");
+    }
+    
+    C_Device[ICE_NET] = device_create(C_Class[ICE_NET], NULL, MKDEV(majorNumber[ICE_NET], 0), NULL, DEVICE_NET);
+    if (IS_ERR(C_Device[ICE_NET]))
+    {
+        class_destroy(C_Class[ICE_NET]);
+        unregister_chrdev(majorNumber[ICE_NET], DEVICE_NET);
+        printk(KERN_ALERT "[INIT][NET] Failed to create the device\n");
+    }
+    else
+    {
+        printk(KERN_ALERT "[INIT][NET] Create char Device\n");
+    }
+
+    /* TODO :: Need data architecture for NET communication */
+    // init_charDevice_Data();
+
+    printk(KERN_ALERT "[INIT][NET] Lock on [C] Device Mutex\n");
+    mutex_init(&net_mutex);
 }
 
 void charDeviceDestroy(void)
 {
-    if(C_Device) 
+    //
+    // iceCOM
+    //
+    if(C_Device[ICE_COM]) 
     {
-        device_destroy(C_Class, MKDEV(majorNumber, 0));
-        C_Device = NULL;
-        printk(KERN_INFO "[DESTROY][ C ] Device destroyed\n");
+        device_destroy(C_Class[ICE_COM], MKDEV(majorNumber[ICE_COM], 0));
+        C_Device[ICE_COM] = NULL;
+        printk(KERN_INFO "[DESTROY][COM] Device destroyed\n");
     }
     else
     {
-        printk(KERN_INFO "[DESTROY][ C ] Canot destroy C_Device :: It is already NULL !\n");
+        printk(KERN_INFO "[DESTROY][COM] Canot destroy C_Device[ICE_COM] :: It is already NULL !\n");
     }
 
-    if(C_Class) 
+    if(C_Class[ICE_COM]) 
     {
-        class_unregister(C_Class);
-        class_destroy(C_Class);
-        C_Class = NULL;
-        printk(KERN_INFO "[DESTROY][ C ] Class destroyed\n");
+        class_unregister(C_Class[ICE_COM]);
+        class_destroy(C_Class[ICE_COM]);
+        C_Class[ICE_COM] = NULL;
+        printk(KERN_INFO "[DESTROY][COM] Class destroyed\n");
     }
     else
     {
-        printk(KERN_INFO "[DESTROY][ C ] Canot destroy C_Class :: It is already NULL !\n");
+        printk(KERN_INFO "[DESTROY][COM] Canot destroy C_Class[ICE_COM] :: It is already NULL !\n");
     }
 
-    if(majorNumber != 0) 
+    if(majorNumber[ICE_COM] != 0) 
     {
-        unregister_chrdev(majorNumber, DEVICE_NAME);
-        majorNumber = 0;
-        printk(KERN_INFO "[DESTROY][ C ] Unregistered character device\n");
+        unregister_chrdev(majorNumber[ICE_COM], DEVICE_COM);
+        majorNumber[ICE_COM] = 0;
+        printk(KERN_INFO "[DESTROY][COM] Unregistered character device\n");
     }
     else
     {
-        printk(KERN_INFO "[DESTROY][ C ] Canot unregister iceCOM Device :: majorNumber is already 0 !\n");
-        printk(KERN_INFO "[DESTROY][ C ] Device destroyed\n");
+        printk(KERN_INFO "[DESTROY][COM] Canot unregister iceCOM Device :: majorNumber[ICE_COM] is already 0 !\n");
+        printk(KERN_INFO "[DESTROY][COM] Device destroyed\n");
     }
 
     mutex_destroy(&com_mutex);
-    printk(KERN_INFO "[DESTROY][ C ] Mutex destroyed\n");
-    printk(KERN_INFO "[DESTROY][ C ] Char device destruction complete\n");
+    printk(KERN_INFO "[DESTROY][COM] Mutex destroyed\n");
+    printk(KERN_INFO "[DESTROY][COM] Char device destruction complete\n");
+
+    //
+    // iceNET
+    //
+    if(C_Device[ICE_NET]) 
+    {
+        device_destroy(C_Class[ICE_NET], MKDEV(majorNumber[ICE_NET], 0));
+        C_Device[ICE_NET] = NULL;
+        printk(KERN_INFO "[DESTROY][NET] Device destroyed\n");
+    }
+    else
+    {
+        printk(KERN_INFO "[DESTROY][NET] Canot destroy C_Device[ICE_NET] :: It is already NULL !\n");
+    }
+
+    if(C_Class[ICE_NET]) 
+    {
+        class_unregister(C_Class[ICE_NET]);
+        class_destroy(C_Class[ICE_NET]);
+        C_Class[ICE_NET] = NULL;
+        printk(KERN_INFO "[DESTROY][NET] Class destroyed\n");
+    }
+    else
+    {
+        printk(KERN_INFO "[DESTROY][NET] Canot destroy C_Class[ICE_NET] :: It is already NULL !\n");
+    }
+
+    if(majorNumber[ICE_NET] != 0) 
+    {
+        unregister_chrdev(majorNumber[ICE_NET], DEVICE_NET);
+        majorNumber[ICE_NET] = 0;
+        printk(KERN_INFO "[DESTROY][NET] Unregistered character device\n");
+    }
+    else
+    {
+        printk(KERN_INFO "[DESTROY][NET] Canot unregister iceNET Device :: majorNumber[ICE_NET] is already 0 !\n");
+        printk(KERN_INFO "[DESTROY][NET] Device destroyed\n");
+    }
+
+    mutex_destroy(&net_mutex);
+    printk(KERN_INFO "[DESTROY][NET] Mutex destroyed\n");
+    printk(KERN_INFO "[DESTROY][NET] Char device destruction complete\n");
 }
 
 static int dev_open(struct inode *inodep, struct file *filep)
 {
     if(!mutex_trylock(&com_mutex))
     {
-        printk(KERN_ALERT "[CTRL][ C ] Device in use by another process");
+        printk(KERN_ALERT "[CTRL][COM] Device in use by another process");
         return -EBUSY;
     }
 
-    numberOpens++;
-    printk(KERN_INFO "[CTRL][ C ] Device has been opened %d time(s)\n", numberOpens);
+    numberOpens[ICE_COM]++;
+    printk(KERN_INFO "[CTRL][COM] Device has been opened %d time(s)\n", numberOpens[ICE_COM]);
 
-    return CD_OK;
+    if(!mutex_trylock(&net_mutex))
+    {
+        printk(KERN_ALERT "[CTRL][NET] Device in use by another process");
+        return -EBUSY;
+    }
+
+    numberOpens[ICE_NET]++;
+    printk(KERN_INFO "[CTRL][NET] Device has been opened %d time(s)\n", numberOpens[ICE_NET]);
+
+    return 0;
 }
 
 static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset)
@@ -193,13 +311,13 @@ static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *of
 
     if (error_count == 0)
     {
-        printk(KERN_INFO "[CTRL][ C ] Sent %zu characters to user-space\n", charDeviceTransfer.length);
+        printk(KERN_INFO "[CTRL][COM] Sent %zu characters to user-space\n", charDeviceTransfer.length);
         /* Length == Preamble + Null Terminator */
         return charDeviceTransfer.length; 
     }
     else 
     {
-        printk(KERN_INFO "[CTRL][ C ] Failed to send %d characters to user-space\n", error_count);
+        printk(KERN_INFO "[CTRL][COM] Failed to send %d characters to user-space\n", error_count);
         /* Failed -- return a bad address message (i.e. -14) */
         return -EFAULT;
     }
@@ -223,9 +341,9 @@ static ssize_t dev_write(struct file *filep, const char __user *buffer, size_t l
 #if 1 /* Test :: Read Enable pulse to FIFO */
     if (charDeviceTransfer.RxData[0] == 0x12 && charDeviceTransfer.RxData[1] == 0x34)
     {
-        printk(KERN_INFO "[CTRL][ C ] Generate FIFO rd_en from Kernel [long pulse] to be cut in FPGA\n");
+        printk(KERN_INFO "[CTRL][COM] Generate FIFO rd_en from Kernel [long pulse] to be cut in FPGA\n");
         setStateMachine(INTERRUPT);
-        return CD_OK;
+        return 0;
     }
 #endif
 
@@ -236,18 +354,18 @@ static ssize_t dev_write(struct file *filep, const char __user *buffer, size_t l
     // Print each character of the RxData array
     for (i = 0; i < charDeviceTransfer.length; i++) 
     {
-        printk(KERN_INFO "[CTRL][ C ] Received Byte[%zu]: 0x%02x\n", i, (unsigned char)charDeviceTransfer.RxData[i]);
+        printk(KERN_INFO "[CTRL][COM] Received Byte[%zu]: 0x%02x\n", i, (unsigned char)charDeviceTransfer.RxData[i]);
     }
 
     setStateMachine(SPI);
 
-    return CD_OK;
+    return 0;
 }
 
 static int dev_release(struct inode *inodep, struct file *filep)
 {
-    printk(KERN_ALERT "[INIT][ C ] Unlock [C] Device Mutex\n");
+    printk(KERN_ALERT "[INIT][COM] Unlock [C] Device Mutex\n");
     mutex_unlock(&com_mutex);
-    printk(KERN_INFO "[CTRL][ C ] Device successfully closed\n");
-    return CD_OK;
+    printk(KERN_INFO "[CTRL][COM] Device successfully closed\n");
+    return 0;
 }
