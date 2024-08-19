70ae9923cd9648d9938f68fceb41af9551410749
IceDefcon <ice.defcon@yahoo.com>
2024-08-20 00:20:40 +0200

    ADXL345 Configuration

---
+++ firmware/app/include/Config.h
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+/**
+ * 
+ * ADXL345 :: Regoister config
+ * 
+ * 
+ * 
+ * 0x2D ---> 0x00 :: It is recommended to configure the device in standby mode
+ * 0x2E ---> 0x00 :: It is recommended that interrupt bits be configured with the interrupts disabled
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 0x31 ---> 0x08 :: Enable Full Resolution (16bit)
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 0x2D ---> 0x08 :: Finish config + Enter Measure mode
+ * 
+ * 
+ * 
+ */
