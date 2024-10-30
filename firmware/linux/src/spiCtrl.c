559d6958cfe7e815891cc261f72bd338953a89f4
IceDefcon <ice.defcon@yahoo.com>
2024-10-30 21:29:42 +0100

    SPI transfers using DMA controller

--- firmware/linux/src/spiCtrl.c
+++ firmware/linux/src/spiCtrl.c
@@ -8,6 +8,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/slab.h>  // For kmalloc and kfree
+#include <linux/dma-mapping.h>  // For DMA support
 
 #include "spiCtrl.h"
 #include "charDevice.h"
@@ -45,55 +46,31 @@ static spiDeviceData Device[SPI_AMOUNT] =
     },
 };
 
-int spiInit(void)
+int spiBusInit(int bus, spiDeviceType device)
 {
-
-    struct spi_master *spi_master_primary;
-    struct spi_master *spi_master_secondary;
+    struct spi_master *pMaster;
     int ret;
 
-    spi_master_primary = spi_busnum_to_master(0);
-    if (!spi_master_primary)
+    pMaster = spi_busnum_to_master(bus);
+    if (!pMaster)
     {
-        printk(KERN_ERR "[INIT][SPI] SPI Master at BUS 0 not found!\n");
+        printk(KERN_ERR "[INIT][SPI] SPI Master at BUS %d not found!\n", bus);
         return -ENODEV;
     }
     else
     {
-        printk(KERN_ERR "[INIT][SPI] SPI Master at BUS 0 Registered\n");
+        printk(KERN_ERR "[INIT][SPI] SPI Master at BUS %d Registered\n", bus);
     }
 
-    spi_master_secondary = spi_busnum_to_master(1);
-    if (!spi_master_secondary)
+    Device[device].spiDevice = spi_alloc_device(pMaster);
+    if (!Device[device].spiDevice)
     {
-        printk(KERN_ERR "[INIT][SPI] SPI Master at BUS 1 not found!\n");
-        return -ENODEV;
-    }
-    else
-    {
-        printk(KERN_ERR "[INIT][SPI] SPI Master at BUS 1 Registered\n");
-    }
-
-    Device[SPI_PRIMARY].spiDevice = spi_alloc_device(spi_master_primary);
-    if (!Device[SPI_PRIMARY].spiDevice)
-    {
-        printk(KERN_ERR "[INIT][SPI] SPI0 Failed to Allocate!\n");
+        printk(KERN_ERR "[INIT][SPI] SPI %d Failed to Allocate!\n", bus);
         return -ENOMEM;
     }
     else
     {
-        printk(KERN_ERR "[INIT][SPI] SPI0 Allocated\n");
-    }
-
-    Device[SPI_SECONDARY].spiDevice = spi_alloc_device(spi_master_secondary);
-    if (!Device[SPI_SECONDARY].spiDevice)
-    {
-        printk(KERN_ERR "[INIT][SPI] SPI1 Failed to Allocate!\n");
-        return -ENOMEM;
-    }
-    else
-    {
-        printk(KERN_ERR "[INIT][SPI] SPI1 Allocated\n");
+        printk(KERN_ERR "[INIT][SPI] SPI %d Allocated\n", bus);
     }
 
     /*!
@@ -102,39 +79,31 @@ int spiInit(void)
      *
      * Only reqired when talking to FPGA
      */
-    Device[SPI_PRIMARY].spiDevice->chip_select = 0;
-    Device[SPI_PRIMARY].spiDevice->mode = SPI_MODE_1; /* For Kernel <=> FPGA Communication */
-    Device[SPI_PRIMARY].spiDevice->bits_per_word = 8;
-    Device[SPI_PRIMARY].spiDevice->max_speed_hz = 1000000;
+    Device[device].spiDevice->chip_select = 0;
+    Device[device].spiDevice->mode = SPI_MODE_1; /* For Kernel <=> FPGA Communication */
+    Device[device].spiDevice->bits_per_word = 8;
+    Device[device].spiDevice->max_speed_hz = 1000000;
 
-    ret = spi_setup(Device[SPI_PRIMARY].spiDevice);
+    ret = spi_setup(Device[device].spiDevice);
     if (ret < 0)
     {
-        printk(KERN_ERR "[INIT][SPI] SPI0 device Failed to setup! ret[%d]\n", ret);
-        spi_dev_put(Device[SPI_PRIMARY].spiDevice);
+        printk(KERN_ERR "[INIT][SPI] SPI %d device Failed to setup! ret[%d]\n", bus, ret);
+        spi_dev_put(Device[device].spiDevice);
         return ret;
     }
     else
     {
-        printk(KERN_ERR "[INIT][SPI] SPI0 device setup\n");
+        printk(KERN_ERR "[INIT][SPI] SPI %d device setup\n", bus);
     }
 
-    Device[SPI_SECONDARY].spiDevice->chip_select = 0;
-    Device[SPI_SECONDARY].spiDevice->mode = SPI_MODE_1; /* For Kernel <=> FPGA Communication */
-    Device[SPI_SECONDARY].spiDevice->bits_per_word = 8;
-    Device[SPI_SECONDARY].spiDevice->max_speed_hz = 1000000;
+    return ret;
+}
 
-    ret = spi_setup(Device[SPI_SECONDARY].spiDevice);
-    if (ret < 0)
-    {
-        printk(KERN_ERR "[INIT][SPI] SPI1 device Failed to setup! ret[%d]\n", ret);
-        spi_dev_put(Device[SPI_SECONDARY].spiDevice);
-        return ret;
-    }
-    else
-    {
-        printk(KERN_ERR "[INIT][SPI] SPI1 device setup\n");
-    }
+
+int spiInit(void)
+{
+    (void)spiBusInit(0, SPI_PRIMARY);
+    (void)spiBusInit(1, SPI_SECONDARY);
 
     return 0;
 }
@@ -143,20 +112,42 @@ void transferFpgaInput(struct work_struct *work)
 {
     struct spi_message msg;
     struct spi_transfer transfer;
+    dma_addr_t tx_dma, rx_dma;
     int ret;
     int i;
 
     DataTransfer* kernelOutptData = getKernelInputTransfer();
 
+    /* Allocate DMA buffers */
+    tx_dma = dma_map_single(Device[SPI_PRIMARY].spiDevice->controller->dev.parent, (void *)kernelOutptData->RxData, Device[SPI_PRIMARY].spiLength, DMA_TO_DEVICE);
+    rx_dma = dma_map_single(Device[SPI_PRIMARY].spiDevice->controller->dev.parent, (void *)Device[SPI_PRIMARY].spiRx, Device[SPI_PRIMARY].spiLength, DMA_FROM_DEVICE);
+
+    if (dma_mapping_error(Device[SPI_PRIMARY].spiDevice->controller->dev.parent, tx_dma) ||
+        dma_mapping_error(Device[SPI_PRIMARY].spiDevice->controller->dev.parent, rx_dma))
+    {
+        printk(KERN_ERR "[CTRL][SPI] DMA mapping failed\n");
+        return;
+    }
+
     memset(&transfer, 0, sizeof(transfer));
-    transfer.tx_buf = (void *)kernelOutptData->RxData;
-    transfer.rx_buf = (void *)Device[SPI_PRIMARY].spiRx;
+    transfer.tx_dma = tx_dma;
+    transfer.rx_dma = rx_dma;
     transfer.len = Device[SPI_PRIMARY].spiLength;
+    transfer.tx_buf = (void *)kernelOutptData->RxData;  /* Assign DMA buffer to tx_buf */
+    transfer.rx_buf = (void *)Device[SPI_PRIMARY].spiRx;  /* Assign DMA buffer to rx_buf */
+    transfer.cs_change = 1;
+    transfer.speed_hz = Device[SPI_PRIMARY].spiDevice->max_speed_hz;
 
     spi_message_init(&msg);
     spi_message_add_tail(&transfer, &msg);
 
+    /* Initate DMA Controller to perform SPI transfer */
     ret = spi_sync(Device[SPI_PRIMARY].spiDevice, &msg);
+
+    /* Unmap DMA buffers */
+    dma_unmap_single(Device[SPI_PRIMARY].spiDevice->controller->dev.parent, tx_dma, Device[SPI_PRIMARY].spiLength, DMA_TO_DEVICE);
+    dma_unmap_single(Device[SPI_PRIMARY].spiDevice->controller->dev.parent, rx_dma, Device[SPI_PRIMARY].spiLength, DMA_FROM_DEVICE);
+
     if (ret < 0)
     {
         printk(KERN_ERR "[CTRL][SPI] SPI transfer at signal From Char Device failed: %d\n", ret);
@@ -164,7 +155,7 @@ void transferFpgaInput(struct work_struct *work)
     }
     else
     {
-        printk(KERN_INFO "[CTRL][SPI] Primary FPGA Transfer :: Signaled by transferFpgaInput over SPI.0");
+        printk(KERN_INFO "[CTRL][SPI] Primary FPGA Transfer :: Signaled by transferFpgaInput over SPI.0\n");
     }
 
     for (i = 0; i < Device[SPI_PRIMARY].spiLength; ++i)
@@ -190,20 +181,42 @@ void transferFpgaOutput(struct work_struct *work)
 {
     struct spi_transfer transfer;
     struct spi_message msg;
+    dma_addr_t tx_dma, rx_dma;
     int ret;
     int i;
 
     DataTransfer* kernelOutptData = getKernelOutputTransfer();
 
+    /* Allocate DMA buffers */
+    tx_dma = dma_map_single(Device[SPI_SECONDARY].spiDevice->controller->dev.parent, (void *)Device[SPI_SECONDARY].spiTx, Device[SPI_SECONDARY].spiLength, DMA_TO_DEVICE);
+    rx_dma = dma_map_single(Device[SPI_SECONDARY].spiDevice->controller->dev.parent, (void *)Device[SPI_SECONDARY].spiRx, Device[SPI_SECONDARY].spiLength, DMA_FROM_DEVICE);
+
+    if (dma_mapping_error(Device[SPI_SECONDARY].spiDevice->controller->dev.parent, tx_dma) ||
+        dma_mapping_error(Device[SPI_SECONDARY].spiDevice->controller->dev.parent, rx_dma))
+    {
+        printk(KERN_ERR "[CTRL][SPI] DMA mapping failed\n");
+        return;
+    }
+
     memset(&transfer, 0, sizeof(transfer));
-    transfer.tx_buf = (void *)Device[SPI_SECONDARY].spiTx;
-    transfer.rx_buf = (void *)Device[SPI_SECONDARY].spiRx;
+    transfer.tx_dma = tx_dma;
+    transfer.rx_dma = rx_dma;
     transfer.len = Device[SPI_SECONDARY].spiLength;
+    transfer.tx_buf = (void *)Device[SPI_SECONDARY].spiTx;  /* Assign DMA buffer to tx_buf */
+    transfer.rx_buf = (void *)Device[SPI_SECONDARY].spiRx;  /* Assign DMA buffer to rx_buf */
+    transfer.cs_change = 1;
+    transfer.speed_hz = Device[SPI_SECONDARY].spiDevice->max_speed_hz;
 
     spi_message_init(&msg);
     spi_message_add_tail(&transfer, &msg);
 
+    /* Initate DMA Controller to perform SPI transfer */
     ret = spi_sync(Device[SPI_SECONDARY].spiDevice, &msg);
+
+    /* Unmap DMA buffers */
+    dma_unmap_single(Device[SPI_SECONDARY].spiDevice->controller->dev.parent, tx_dma, Device[SPI_SECONDARY].spiLength, DMA_TO_DEVICE);
+    dma_unmap_single(Device[SPI_SECONDARY].spiDevice->controller->dev.parent, rx_dma, Device[SPI_SECONDARY].spiLength, DMA_FROM_DEVICE);
+
     if (ret < 0)
     {
         printk(KERN_ERR "[CTRL][SPI] SPI transfer at interrupt From Fpga failed: %d\n", ret);
@@ -211,7 +224,7 @@ void transferFpgaOutput(struct work_struct *work)
     }
     else
     {
-        printk(KERN_INFO "[CTRL][SPI] Secondary FPGA Transfer :: Signaled by transferFpgaOutput over SPI.1");
+        printk(KERN_INFO "[CTRL][SPI] Secondary FPGA Transfer :: Signaled by transferFpgaOutput over SPI.1\n");
     }
 
     for (i = 0; i < Device[SPI_SECONDARY].spiLength; ++i)
@@ -247,6 +260,6 @@ void killApplication(struct work_struct *work)
 void spiDestroy(void)
 {
     spi_dev_put(Device[SPI_PRIMARY].spiDevice);
-	spi_dev_put(Device[SPI_SECONDARY].spiDevice);
+    spi_dev_put(Device[SPI_SECONDARY].spiDevice);
     printk(KERN_INFO "[DESTROY][SPI] Destroy SPI Devices\n");
 }
