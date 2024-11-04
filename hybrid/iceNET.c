e2580a69f4fd560f0e9adbb47d7a97b047f527d8
IceDefcon <ice.defcon@yahoo.com>
2024-11-04 20:53:12 +0100

    Hybrid platform :: Ultra96v2

---
+++ hybrid/iceNET.c
@@ -0,0 +1,63 @@
+/*!
+ *
+ * Author: Ice.Marek
+ * IceNET Technology 2024
+ *
+ */
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_VERSION("1.0");
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Ice Marek");
+MODULE_DESCRIPTION("IceNET Driver");
+
+//////////////////////////
+//                      //
+//        [FPGA]        //
+//        DRIVER        //
+//         INIT         //
+//                      //
+//////////////////////////
+
+static int __init iceNET_init(void)
+{
+    printk(KERN_INFO "----------------------------------\n");
+    printk(KERN_INFO "[BEGIN] IceNET CPU & FPGA Platform\n");
+    printk(KERN_INFO "----------------------------------\n");
+
+    /**
+     *
+     * CODE
+     * CODE
+     * CODE
+     * CODE
+     * CODE
+     *
+     */
+
+    printk(KERN_INFO "----------------------------------\n");
+    printk(KERN_INFO "[READY] Driver loaded successfuly \n");
+    printk(KERN_INFO "----------------------------------\n");
+
+    return 0;
+}
+
+static void __exit iceNET_exit(void)
+{
+    /**
+     *
+     * CODE
+     * CODE
+     * CODE
+     * CODE
+     * CODE
+     *
+     */
+
+    printk(KERN_INFO "[TERMINATE] Driver terminated successfully\n");
+}
+
+module_init(iceNET_init);
+module_exit(iceNET_exit);
