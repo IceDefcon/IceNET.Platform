4d0625bc3649170bb4d0e3a43b098300ebd09679
IceDefcon <ice.defcon@yahoo.com>
2024-12-09 10:21:56 +0100

    Tidy-up

--- hybrid/.iceNET.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/ice/code.lab/IceNET/IceNET.Platform/hybrid/iceNET.mod := { echo  /ice/code.lab/IceNET/IceNET.Platform/hybrid/iceNET.o;  echo; } > /ice/code.lab/IceNET/IceNET.Platform/hybrid/iceNET.mod
--- hybrid/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/ice/code.lab/IceNET/IceNET.Platform/hybrid/modules.order := {   echo /ice/code.lab/IceNET/IceNET.Platform/hybrid/iceNET.ko; :; } | awk '!x[$$0]++' - > /ice/code.lab/IceNET/IceNET.Platform/hybrid/modules.order
