32f0f8fb2eca57c1947aec75b184044182084b8d
IceDefcon <ice.defcon@yahoo.com>
2025-01-23 00:39:32 +0100

    Kernel Comms :: Part 1

--- firmware/app/src/main.cpp
+++ firmware/app/src/main.cpp
@@ -6,73 +6,29 @@
  */
 #include <chrono> // delay
 #include <thread> // delay
-
-// #include "NetworkTraffic.h"
-// #include "Output.h"
-// #include "Input.h"
-// #include "RamConfig.h"
-// #include "ServerTCP.h"
-// #include "Commander.h"
-// #include "Watchdog.h"
-
 #include "DroneCtrl.h"
 
-int main() 
+int main()
 {
-#if 0
-    /* Smart pointers for auto Heap allocation and dealocation */
-    auto instanceNetworkTraffic = std::make_shared<NetworkTraffic>();
-    auto instanceWatchdog = std::make_shared<Watchdog>();
-    auto instanceInput = std::make_shared<Input>();
-    auto instanceRamConfig = std::make_shared<RamConfig>();
-    auto instanceOutput = std::make_shared<Output>();
-    auto instanceServerTCP = std::make_shared<ServerTCP>();
-    auto instanceCommander = std::make_shared<Commander>();
-
-    /* Set Instances */
-    instanceOutput->setInstance_NetworkTraffic(instanceNetworkTraffic);
-    instanceServerTCP->setInstance_NetworkTraffic(instanceNetworkTraffic);
-    instanceServerTCP->setInstance_RamConfig(instanceRamConfig);
-    instanceNetworkTraffic->setInstance_Input(instanceInput);
-    instanceWatchdog->setInstance_RamConfig(instanceRamConfig);
-    instanceWatchdog->setInstance_Commander(instanceCommander);
-
-    /* Initialize Interfaces */
-    instanceCommander->openDEV();
-    instanceWatchdog->openDEV();
-    instanceInput->openDEV();
-    instanceOutput->openDEV();
-    instanceServerTCP->openDEV();
-    instanceNetworkTraffic->openDEV();
-
-    while (true) /* Terminate Kernel comms and Clean Memory */
-    {
-        if (instanceOutput->isThreadKilled() || instanceWatchdog->isThreadKilled())
-        {
-            instanceWatchdog->closeDEV();
-            instanceServerTCP->closeDEV();
-            instanceOutput->closeDEV();
-            instanceInput->closeDEV();
-            instanceNetworkTraffic->closeDEV();
-            instanceCommander->closeDEV();
-            break;
-        }
-
-        /* Reduce consumption of CPU resources */
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    }
-#endif
-
+    /**
+     * Smart pointer for auto Heap
+     * allocation and dealocation
+     */
     auto instanceDroneCtrl = std::make_shared<DroneCtrl>();
-    instanceDroneCtrl->Commander::openDEV();
-    instanceDroneCtrl->Watchdog::openDEV();
+
+    instanceDroneCtrl->KernelComms::Commander::openDEV();
+    instanceDroneCtrl->KernelComms::Watchdog::openDEV();
+    instanceDroneCtrl->KernelComms::Input::openDEV();
+    instanceDroneCtrl->KernelComms::Output::openDEV();
 
     while (true) /* Terminate Kernel comms and Clean Memory */
     {
         if (instanceDroneCtrl->Output::isThreadKilled() || instanceDroneCtrl->Watchdog::isThreadKilled())
         {
-            instanceDroneCtrl->Watchdog::closeDEV();
-            instanceDroneCtrl->Commander::closeDEV();
+            instanceDroneCtrl->KernelComms::Watchdog::closeDEV();
+            instanceDroneCtrl->KernelComms::Output::closeDEV();
+            instanceDroneCtrl->KernelComms::Input::closeDEV();
+            instanceDroneCtrl->KernelComms::Commander::closeDEV();
             break;
         }
 
@@ -83,3 +39,23 @@ int main()
     /* shared_ptr in use :: No need for deallocation */
     return 0;
 }
+
+#if 0 /* Old Costruction */
+
+    /* Initialize Interfaces */
+    instanceCommander->openDEV();
+    instanceWatchdog->openDEV();
+    instanceInput->openDEV();
+    instanceOutput->openDEV();
+    instanceServerTCP->openDEV();
+    instanceNetworkTraffic->openDEV();
+
+    /* Terminate Kernel comms and Clean Memory */
+    instanceWatchdog->closeDEV();
+    instanceServerTCP->closeDEV();
+    instanceOutput->closeDEV();
+    instanceInput->closeDEV();
+    instanceNetworkTraffic->closeDEV();
+    instanceCommander->closeDEV();
+
+#endif
