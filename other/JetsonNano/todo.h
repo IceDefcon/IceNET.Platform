24333bc5a037c7df3fb96b5175178c499430c15e
IceDefcon <ice.defcon@yahoo.com>
2025-03-10 22:46:29 +0100

    New installation :: TODO

---
+++ other/JetsonNano/todo.h
@@ -0,0 +1,63 @@
+//
+// Sublime-text
+//
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo gpg --dearmor -o /usr/share/keyrings/sublimehq-pub.gpg
+echo "deb [signed-by=/usr/share/keyrings/sublimehq-pub.gpg] https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install sublime-text
+
+    "trim_trailing_white_space_on_save": true,
+    "ensure_newline_at_eof_on_save": true,
+    "translate_tabs_to_spaces": true,
+    "tab_size": 4,
+    "auto_indent": true,
+    "detect_indentation": true,
+    "trim_only_modified_white_space": true,
+    "ensure_single_trailing_newline": true,
+
+sudo apt-get install sublime-merge
+
+//
+// Pip
+//
+sudo apt update
+sudo apt install python3-pip -y
+
+//
+// jtop
+//
+sudo -H pip3 install --no-cache-dir -U jetson-stats
+
+//
+// Git
+//
+git config --global user.email "ice.defcon@yahoo.com"
+git config --global user.name "IceDefcon"
+git config --global credential.helper 'store --file ~/.git-credentials'
+
+//
+// Qt5
+//
+sudo apt update
+sudo apt install qt5-qmake qtbase5-dev
+export PATH=/usr/lib/qt5/bin:$PATH
+
+//
+// Bash
+//
+Create folder /tmp/runtime-root
+
+cd /home/ice/code.lab/IceNET.Platform
+alias push_droneAI="git push origin droneAI"
+export XDG_RUNTIME_DIR="/tmp/runtime-root"
+export PATH=/usr/lib/qt5/bin:$PATH
+
+//
+// Enable SPI trough device tree
+//
+copy into /boot/nano-spi.dtb
+
+//
+// Install
+//
+apt-get install guake nano
