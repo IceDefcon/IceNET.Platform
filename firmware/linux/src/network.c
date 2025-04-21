025eb57fce469a86bae85da5a7a681740237d9e7
IceDefcon <ice.defcon@yahoo.com>
2025-04-21 15:44:53 +0200

    [CPU] ARP support for Drone :: Part 1

--- firmware/linux/src/network.c
+++ firmware/linux/src/network.c
@@ -16,6 +16,8 @@
 #include <linux/netdevice.h>   // L2 - Network device structures (Ethernet)
 #include <linux/inet.h>
 #include <linux/etherdevice.h> // L2 - Ethernet header helpers
+#include <linux/if_arp.h>
+#include <linux/if_ether.h>
 #include <linux/crypto.h>
 #include <linux/scatterlist.h>
 
@@ -26,12 +28,13 @@
 #define AES_BLOCK_SIZE 16
 
 static struct nf_hook_ops netFilterHook;
+static struct packet_type arp_packet_type;
 
 typedef struct
 {
     struct ethhdr *ethernetHeader;
 
-}hookControlType;
+} hookControlType;
 
 static hookControlType hookControl =
 {
@@ -68,7 +71,47 @@ static int aes_decrypt(void *data, size_t len, u8 *key, u8 *iv)
     return 0;
 }
 
-// Hook function - processes L2, L3, and L4
+// ARP packet handler
+static int handle_arp_request(struct sk_buff *skb)
+{
+    struct arphdr *arp;
+    unsigned char *arp_ptr;
+    unsigned char *sha, *spa, *tha, *tpa;
+
+    arp = arp_hdr(skb);
+    if (!arp)
+        return NET_RX_DROP;
+
+    if (ntohs(arp->ar_op) != ARPOP_REQUEST)
+        return NET_RX_SUCCESS;
+
+    arp_ptr = (unsigned char *)(arp + 1);
+    sha = arp_ptr;
+    spa = sha + arp->ar_hln;
+    tha = spa + arp->ar_pln;
+    tpa = tha + arp->ar_hln;
+
+    pr_info("[ARP] ARP request: who has %pI4? Tell %pI4\n", tpa, spa);
+
+    return NET_RX_SUCCESS;
+}
+
+static int arp_receive(struct sk_buff *skb, struct net_device *dev,
+                       struct packet_type *pt, struct net_device *orig_dev)
+{
+    if (!skb)
+        return NET_RX_DROP;
+
+    skb = skb_share_check(skb, GFP_ATOMIC);
+    if (!skb)
+        return NET_RX_DROP;
+
+    handle_arp_request(skb);
+    kfree_skb(skb);
+    return NET_RX_SUCCESS;
+}
+
+// Main Netfilter hook
 static unsigned int receiverHook(void *priv, struct sk_buff *socketBuffer, const struct nf_hook_state *state)
 {
     struct iphdr *iph;
@@ -78,7 +121,6 @@ static unsigned int receiverHook(void *priv, struct sk_buff *socketBuffer, const
     int payload_len;
     unsigned short dport;
 
-    // AES decryption key (make sure this is the same key as used in the sending module)
     u8 aes_key[16] =
     {
         0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
@@ -100,18 +142,18 @@ static unsigned int receiverHook(void *priv, struct sk_buff *socketBuffer, const
         }
     }
 
-    // L2 - Ethernet header
     if (skb_mac_header_was_set(socketBuffer))
     {
         hookControl.ethernetHeader = eth_hdr(socketBuffer);
         if (hookControl.ethernetHeader)
         {
             pr_info("[L2][ETH] Src MAC: %pM -> Dst MAC: %pM | Proto: 0x%04x\n",
-                    hookControl.ethernetHeader->h_source, hookControl.ethernetHeader->h_dest, ntohs(hookControl.ethernetHeader->h_proto));
+                    hookControl.ethernetHeader->h_source,
+                    hookControl.ethernetHeader->h_dest,
+                    ntohs(hookControl.ethernetHeader->h_proto));
         }
     }
 
-    // L3 - IP header
     iph = ip_hdr(socketBuffer);
     if (!iph)
         return NF_ACCEPT;
@@ -119,19 +161,16 @@ static unsigned int receiverHook(void *priv, struct sk_buff *socketBuffer, const
     pr_info("[L3][IP] Src IP: %pI4 -> Dst IP: %pI4 | Proto: %d\n",
             &iph->saddr, &iph->daddr, iph->protocol);
 
-    // L4 - UDP
     if (iph->protocol == IPPROTO_UDP)
     {
         udph = udp_hdr(socketBuffer);
         if (!udph)
             return NF_ACCEPT;
 
-        // Filter UDP by allowed ports
         dport = ntohs(udph->dest);
         if (dport != LISTEN_PORT && dport != TEST_PORT)
             return NF_ACCEPT;
 
-        // Check for broadcast IP
         if (iph->daddr != htonl(BROADCAST_IP))
             return NF_ACCEPT;
 
@@ -143,8 +182,6 @@ static unsigned int receiverHook(void *priv, struct sk_buff *socketBuffer, const
         if (payload_len > 0 && skb_tail_pointer(socketBuffer) >= payload + payload_len)
         {
             pr_info("[L7][DATA][UDP] Encrypted Payload: %.32s\n", payload);
-
-            // Decrypt the payload using AES
             if (aes_decrypt(payload, payload_len, aes_key, NULL) == 0)
             {
                 pr_info("[L7][DATA][UDP] Decrypted Payload: %.32s\n", payload);
@@ -159,7 +196,6 @@ static unsigned int receiverHook(void *priv, struct sk_buff *socketBuffer, const
             pr_info("[L7][DATA][UDP] Payload not safely accessible\n");
         }
     }
-    // L4 - TCP
     else if (iph->protocol == IPPROTO_TCP)
     {
         tcph = (struct tcphdr *)((__u32 *)iph + iph->ihl);
@@ -175,8 +211,6 @@ static unsigned int receiverHook(void *priv, struct sk_buff *socketBuffer, const
         if (payload_len > 0 && skb_tail_pointer(socketBuffer) >= payload + payload_len)
         {
             pr_info("[L7][DATA][TCP] Encrypted Payload: %.32s\n", payload);
-
-            // Decrypt the payload using AES
             if (aes_decrypt(payload, payload_len, aes_key, NULL) == 0)
             {
                 pr_info("[L7][DATA][TCP] Decrypted Payload: %.32s\n", payload);
@@ -198,6 +232,10 @@ static unsigned int receiverHook(void *priv, struct sk_buff *socketBuffer, const
 // Init routine
 void broadcastRxInit(void)
 {
+    /**
+     * [L3] Netfilter hook works only at Layer 3
+     * and above meaning IP packets (IPv4, IPv6, etc)
+     */
     netFilterHook.hook = receiverHook;
     netFilterHook.hooknum = NF_INET_PRE_ROUTING;
     netFilterHook.pf = PF_INET;
@@ -211,12 +249,21 @@ void broadcastRxInit(void)
     {
         pr_info("[INIT][NET] Sniffing TCP and UDP @ port %d\n", LISTEN_PORT);
     }
+
+    /**
+     * [L2] ARP is not IP protocol
+     * ARP is Layer 2 protocol instead
+     */
+    arp_packet_type.type = htons(ETH_P_ARP);
+    arp_packet_type.func = arp_receive;
+    dev_add_pack(&arp_packet_type);
+    pr_info("[INIT][ARP] ARP sniffer registered\n");
 }
 
 // Cleanup routine
 void broadcastRxDestroy(void)
 {
     nf_unregister_net_hook(&init_net, &netFilterHook);
+    dev_remove_pack(&arp_packet_type);
     pr_info("[DESTROY][NET] Sniffer stopped\n");
 }
-
