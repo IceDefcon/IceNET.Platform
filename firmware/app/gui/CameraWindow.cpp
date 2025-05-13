7394ecc6936dc2f3c4bc314cce11c09a47c90f03
IceDefcon <ice.defcon@yahoo.com>
2025-05-13 15:04:18 +0200

    [CPU] Stream 1280x72030@Hz Using Motion-JPEG over Wi-fi

--- firmware/app/gui/CameraWindow.cpp
+++ firmware/app/gui/CameraWindow.cpp
@@ -21,7 +21,7 @@ CameraWindow::CameraWindow() :
     m_cameraDisplay->setStyleSheet("background-color: black");
 
     /* Open the default camera -> /dev/video0 */
-    m_cap.open(0);
+    m_cap.open("/dev/video0");
 
     if (!m_cap.isOpened())
     {
@@ -29,20 +29,48 @@ CameraWindow::CameraWindow() :
         return;
     }
 
-    /* Set a lower resolution -> 320x240, 640x480 */
-    m_cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
-    m_cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+    /**
+     * v4l2-ctl --list-formats-ext
+     *
+     * Pixel Format: 'MJPG' (compressed)
+     * Name        : Motion-JPEG
+     * Size: Discrete 1280x720 :: Interval: Discrete 0.033s (30.000 fps)
+     * Size: Discrete 640x480 :: Interval: Discrete 0.033s (30.000 fps)
+     * Size: Discrete 320x240 :: Interval: Discrete 0.033s (30.000 fps)
+     *
+     * Pixel Format: 'YUYV'
+     * Name        : YUYV 4:2:2
+     * Size: Discrete 1280x720 :: Interval: Discrete 0.100s (10.000 fps)
+     * Size: Discrete 640x480 :: Interval: Discrete 0.033s (30.000 fps)
+     * Size: Discrete 320x240 :: Interval: Discrete 0.033s (30.000 fps)
+     *
+     */
+    m_cap.set(cv::CAP_PROP_FRAME_WIDTH, 1280);
+    m_cap.set(cv::CAP_PROP_FRAME_HEIGHT, 720);
+    /**
+     * By default we have:
+     *
+     * m_cap.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('Y', 'U', 'Y', 'V'));
+     *
+     */
+    m_cap.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
 
     double actualWidth = m_cap.get(cv::CAP_PROP_FRAME_WIDTH);
     double actualHeight = m_cap.get(cv::CAP_PROP_FRAME_HEIGHT);
     qDebug() << "[CameraWindow] Actual camera resolution:" << actualWidth << "x" << actualHeight;
+
+    int fourcc = m_cap.get(cv::CAP_PROP_FOURCC);
+    qDebug() << "Camera FourCC: " << fourcc;
+
+    double fps = m_cap.get(cv::CAP_PROP_FPS);
+    qDebug() << "Camera FPS: " << fps;
 
     /* GStreamer pipeline */
     std::string gstPipeline = "appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=ultrafast "
         "! rtph264pay config-interval=1 pt=96 ! udpsink host=192.168.8.101 port=5000";
 
     /* Open GStreamer writer */
-    m_writer.open(gstPipeline, 0, 30, cv::Size(640, 480), true);
+    m_writer.open(gstPipeline, 0, 30, cv::Size(1280, 720), true);
 
     if (!m_writer.isOpened())
     {
@@ -79,7 +107,7 @@ void CameraWindow::updateCameraFrame()
     if (m_cap.read(frame))
     {
         /* Force resize to 640x480 before sending */
-        cv::resize(frame, frame, cv::Size(640, 480));
+        cv::resize(frame, frame, cv::Size(1280, 720));
 
         qDebug() << "[CameraWindow] Sending frame:" << frame.cols << "x" << frame.rows;
 
