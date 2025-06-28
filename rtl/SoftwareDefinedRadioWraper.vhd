96c8cd246674d88057c62d73fc14a1db075ecfc0
IceDefcon <ice.defcon@yahoo.com>
2025-06-28 03:10:32 +0200

    [FPGA] SDR Wrapper :: Part 2 (Input signals)

--- rtl/SoftwareDefinedRadioWraper.vhd
+++ rtl/SoftwareDefinedRadioWraper.vhd
@@ -16,38 +16,57 @@ Port
     ADC_12_BITS_I : in  std_logic_vector(11 downto 0); -- Real
     ADC_12_BITS_Q : in  std_logic_vector(11 downto 0); -- Imaginary
     -- OUT
-    COMPLEMENT_XK_RE out  std_logic_vector(15 downto 0);
-    COMPLEMENT_XK_IM out  std_logic_vector(15 downto 0);
-    FLOAT_XK_RE out  std_logic_vector(31 downto 0);
-    FLOAT_XK_IM out  std_logic_vector(31 downto 0)
+    COMPLEMENT_XK_RE : out std_logic_vector(15 downto 0);
+    COMPLEMENT_XK_IM : out std_logic_vector(15 downto 0);
+    FLOAT_XK_RE : out std_logic_vector(31 downto 0);
+    FLOAT_XK_IM : out std_logic_vector(31 downto 0)
 );
 end entity SoftwareDefinedRadioWraper;
 
 architecture rtl of SoftwareDefinedRadioWraper is
 
 ----------------------------------------------------------------------------------------------------------------
--- Synced :: Asynchronic Input Signals
+-- Input Signals
 ----------------------------------------------------------------------------------------------------------------
 
-signal complement_real : std_logic_vector(15 downto 0) := (others => '0');
-signal complement_imag : std_logic_vector(15 downto 0) := (others => '0');
-
+signal fft_input_two_complement_real : std_logic_vector(15 downto 0) := (others => '0');
+signal fft_input_two_complement_imag : std_logic_vector(15 downto 0) := (others => '0');
 signal fft_start : std_logic := '0';
 signal fft_unload : std_logic := '0';
-signal fft_size : std_logic_vector(4 downto 0) := "00000";
 signal fft_write_enable : std_logic := '0';
-signal fft_forward_inversion : std_logic := '0'; -- Indicating forward inversion
+-- Indicating forward inversion
+signal fft_forward_inversion : std_logic := '0';
 signal fft_forward_inversion_write_enable : std_logic := '0';
-
+-- NFFT = log₂(1024) = 10 ---> Which is "01010"
+constant FFT_NFFT : std_logic_vector(4 downto 0) := "01010";
 ----------------------------------------------------------------------------------------------------------------
 --
--- Required only for the fixed values (two's complement)
+-- SCALE_SCH=2×ceil(5)=2×5=10 bits
+-- An example scaling schedule for N =1024, Radix-4,
 --
---
+-- From Datasheet
+-- Burst I/O is [1 0 2 3 2] (ordered from last to first stage)
 --
 ----------------------------------------------------------------------------------------------------------------
-signal fft_scaling_schedule : std_logic_vector(1 downto 0) := "00";
+constant FFT_SCALING_SCHEDULE : std_logic_vector(9 downto 0) := "0100101110";
 
+signal fft_scaling_schedule_write_enable : std_logic := '0';
+----------------------------------------------------------------------------------------------------------------
+--
+-- 1024 64  1/16    Low-delay channel, common
+-- 1024 128 1/8     Typical in WiFi, LTE
+-- 1024 256 1/4     Long multipath channel
+-- 1024 0           No prefix (not recommended for OFDM)
+--
+-- We are not using FFT for communication ---> So (others => '0')
+--
+----------------------------------------------------------------------------------------------------------------
+signal fft_cyclic_prefix_length : std_logic_vector(9 downto 0) := (others => '0');
+signal fft_cyclic_prefix_write_enable : std_logic := '0';
+-- Clock and Reet
+signal fft_master_synchronous_reset : std_logic := '0';
+signal fft_clock_enable : std_logic := '0';
+signal fft_clock : std_logic := '0';
 
 type SDR_SM is
 (
@@ -68,8 +87,9 @@ Port
 (
     CLOCK_250MHz : in  std_logic;
     RESET        : in  std_logic;
-
+    -- IN
     I_12_BITS    : in  std_logic_vector(11 downto 0);
+    -- OUT
     O_16_BITS    : out std_logic_vector(15 downto 0)
 );
 end component;
@@ -79,8 +99,9 @@ Port
 (
     CLOCK_250MHz : in  std_logic;
     RESET        : in  std_logic;
-
+    -- IN
     FIXED_IN  : in  std_logic_vector(15 downto 0);  -- 16-bit 2's complement
+    -- OUT
     FLOAT_OUT : out std_logic_vector(31 downto 0)   -- 32-bit IEEE 754 float
 );
 end component;
@@ -91,29 +112,71 @@ end component;
 
 begin
 
-    state_machine_process:
-    process(CLOCK_50MHz, RESET)
-    begin
-        if RESET = '1' then
-            sdr_state <= SDR_IDLE
-        elsif rising_edge(CLOCK_50MHz) then
+----------------------------------------------------------------------------------------------------------------
+-- INSTANTIAIONS
+----------------------------------------------------------------------------------------------------------------
 
-            case sdr_state is
-                when SDR_IDLE =>
+TwoComplementConverter_module: TwoComplementConverter
+port map
+(
+    CLOCK_250MHz => CLOCK_250MHz,
+    RESET => RESET,
+    -- IN
+    I_12_BITS => (others => '0'),
+    -- OUT
+    O_16_BITS => open
+);
 
-                when SDR_INIT =>
+SinglePrecisionFloatConverter_module: SinglePrecisionFloatConverter
+port map
+(
+    CLOCK_250MHz => CLOCK_250MHz,
+    RESET => RESET,
+    -- IN
+    FIXED_IN => (others => '0'),
+    -- OUT
+    FLOAT_OUT => open
+);
 
-                when SDR_CONFIG =>
+----------------------------------------------------------------------------------------------------------------
+-- PROCESSES
+----------------------------------------------------------------------------------------------------------------
 
-                when SDR_PROCESS =>
+state_machine_process:
+process(CLOCK_250MHz, RESET)
+begin
+    if RESET = '1' then
+        fft_input_two_complement_real <= (others => '0');
+        fft_input_two_complement_imag <= (others => '0');
+        fft_start <= '0';
+        fft_unload <= '0';
+        fft_write_enable <= '0';
+        fft_forward_inversion <= '0';
+        fft_forward_inversion_write_enable <= '0';
+        fft_scaling_schedule_write_enable <= '0';
+        fft_cyclic_prefix_length <= (others => '0');
+        fft_cyclic_prefix_write_enable <= '0';
+        fft_master_synchronous_reset <= '0';
+        fft_clock_enable <= '0';
+        fft_clock <= '0';
+        sdr_state <= SDR_IDLE;
+    elsif rising_edge(CLOCK_250MHz) then
 
-                when SDR_DONE =>
+        case sdr_state is
+            when SDR_IDLE =>
 
-                when others =>
-                    sdr_state <= SDR_IDLE;
-            end case;
-        end if;
-    end process interrupt_process;
+            when SDR_INIT =>
 
+            when SDR_CONFIG =>
+
+            when SDR_PROCESS =>
+
+            when SDR_DONE =>
+
+            when others =>
+                sdr_state <= SDR_IDLE;
+        end case;
+    end if;
+end process;
 
 end architecture rtl;
