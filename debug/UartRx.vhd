1f9e0d1041270a2213a62a26a6cf70870bd318bb
IceDefcon <ice.defcon@yahoo.com>
2025-08-18 11:19:16 +0200

    [FPGA] UART Receiver

--- debug/UartRx.vhd
+++ debug/UartRx.vhd
@@ -19,7 +19,21 @@ end UartRx;
 
 architecture rtl of UartRx is
 
-constant baudRate : std_logic_vector(7 downto 0) := "00011000"; -- 1/[25*20ns] ---> 2M Baud Rate
+constant baudRate : std_logic_vector(7 downto 0) := "00011000"; -- 1/[25*20ns] ---> 2M Baud
+constant bit_baud : integer range 0 to 32 := 25; -- 25*20ns ---> 2M Baud
+constant bit_start : integer range 0 to 32 := 10;
+constant bit_1 : integer range 0 to 32 := bit_start + bit_baud;
+constant bit_2 : integer range 0 to 32 := bit_1 + bit_baud;
+constant bit_3 : integer range 0 to 32 := bit_2 + bit_baud;
+constant bit_4 : integer range 0 to 32 := bit_3 + bit_baud;
+constant bit_5 : integer range 0 to 32 := bit_4 + bit_baud;
+constant bit_6 : integer range 0 to 32 := bit_5 + bit_baud;
+constant bit_7 : integer range 0 to 32 := bit_6 + bit_baud;
+constant bit_stop : integer range 0 to 32 := bit_7 + bit_baud;
+
+signal status_counter : std_logic_vector(8 downto 1) := (others)
+
+signal bit_count : std_logic_vector(3 downto 0) := "0000";
 
 type STATE is
 (
@@ -33,28 +47,27 @@ type STATE is
 );
 signal uart_state: STATE := UART_IDLE;
 
-signal FPGA_UART_RX_d1 : std_logic := '0';
-signal FPGA_UART_RX_d2 : std_logic := '0';
-signal FPGA_UART_RX_d3 : std_logic := '0';
-signal FPGA_UART_RX_d4 : std_logic := '0';
-signal FPGA_UART_RX_delayed : std_logic := '0';
+--signal FPGA_UART_RX_d1 : std_logic := '0';
+--signal FPGA_UART_RX_d2 : std_logic := '0';
+--signal FPGA_UART_RX_d3 : std_logic := '0';
+--signal FPGA_UART_RX_d4 : std_logic := '0';
+--signal FPGA_UART_RX_delayed : std_logic := '0';
 
-signal REG_FPGA_UART_RX : std_logic_vector(2047 downto 0) := (others => '0');
+--signal REG_FPGA_UART_RX : std_logic_vector(2047 downto 0) := (others => '0');
 
-begin
+--begin
 
-buffer_delay_process:
-process(CLOCK)
-begin
-    if rising_edge(CLOCK) then
-        FPGA_UART_RX_d1 <= FPGA_UART_RX;
-        FPGA_UART_RX_d2 <= FPGA_UART_RX_d1;
-        FPGA_UART_RX_d3 <= FPGA_UART_RX_d2;
-        FPGA_UART_RX_d4 <= FPGA_UART_RX_d3;
-        FPGA_UART_RX_delayed <= FPGA_UART_RX_d4;
-    end if;
-end process;
-
+--buffer_delay_process:
+--process(CLOCK)
+--begin
+--    if rising_edge(CLOCK) then
+--        FPGA_UART_RX_d1 <= FPGA_UART_RX;
+--        FPGA_UART_RX_d2 <= FPGA_UART_RX_d1;
+--        FPGA_UART_RX_d3 <= FPGA_UART_RX_d2;
+--        FPGA_UART_RX_d4 <= FPGA_UART_RX_d3;
+--        FPGA_UART_RX_delayed <= FPGA_UART_RX_d4;
+--    end if;
+--end process;
 
 state_machine_process:
 process(CLOCK, RESET)
@@ -89,6 +102,17 @@ begin
                 uart_state <= UART_IDLE;
 
         end case;
+
+
+        if FPGA_UART_RX = '1' then
+            detected_start <= '1';
+        end if;
+
+        if detected_start = '1' then
+            status_counter <= status_counter + '1';
+        elsif detected_start = '1' and detected_stop = '1' then
+            status_counter <= (others => '0');
+        end if;
 
 
     end if;
