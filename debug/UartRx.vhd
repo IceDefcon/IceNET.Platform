36ca16be4d699788902312301e8800b85d066e6f
IceDefcon <ice.defcon@yahoo.com>
2025-09-09 16:09:10 +0200

    Make UART Baudrate compatybile with 500K at 50MHz and 2M at 200MHz

--- debug/UartRx.vhd
+++ debug/UartRx.vhd
@@ -29,19 +29,22 @@ constant PROCESS_SIZE : integer := 2048;
 constant EDGE_COUNT_OFFSET : integer range 0 to 4 := 2;
 constant EDGE_OFFSET : integer range 0 to 4 := 2;
 constant EDGE_SYNC : integer range 0 to 4 := 2;
-
-constant BIT_BAUD : integer range 0 to 64 := 25; -- 25*20ns ---> 2M Baud
-constant BIT_START : integer range 0 to 64 := 15;               -- 15
-constant BIT_0 : integer range 0 to PROCESS_SIZE := BIT_START + BIT_BAUD; -- 40
-constant BIT_1 : integer range 0 to PROCESS_SIZE := BIT_0 + BIT_BAUD;     -- 65
-constant BIT_2 : integer range 0 to PROCESS_SIZE := BIT_1 + BIT_BAUD;     -- 90
-constant BIT_3 : integer range 0 to PROCESS_SIZE := BIT_2 + BIT_BAUD;     -- 115
-constant BIT_4 : integer range 0 to PROCESS_SIZE := BIT_3 + BIT_BAUD;     -- 140
-constant BIT_5 : integer range 0 to PROCESS_SIZE := BIT_4 + BIT_BAUD;     -- 165
-constant BIT_6 : integer range 0 to PROCESS_SIZE := BIT_5 + BIT_BAUD;     -- 190
-constant BIT_7 : integer range 0 to PROCESS_SIZE := BIT_6 + BIT_BAUD;     -- 215
-constant BIT_STOP : integer range 0 to PROCESS_SIZE := BIT_7 + BIT_BAUD;  -- 240
-constant BIT_NEXT : integer range 0 to PROCESS_SIZE := BIT_STOP + BIT_BAUD;   -- 265
+---------------------------------------------------------------------------------------------------
+-- 100*20ns ---> 500K Baud
+-- 100*5ns  ---> 2M Baud
+---------------------------------------------------------------------------------------------------
+constant BIT_BAUD : integer range 0 to 128 := 100;
+constant BIT_START : integer range 0 to 128 := 60; -- 60
+constant BIT_0 : integer range 0 to PROCESS_SIZE := BIT_START + BIT_BAUD; -- 160
+constant BIT_1 : integer range 0 to PROCESS_SIZE := BIT_0 + BIT_BAUD;     -- 260
+constant BIT_2 : integer range 0 to PROCESS_SIZE := BIT_1 + BIT_BAUD;     -- 360
+constant BIT_3 : integer range 0 to PROCESS_SIZE := BIT_2 + BIT_BAUD;     -- 460
+constant BIT_4 : integer range 0 to PROCESS_SIZE := BIT_3 + BIT_BAUD;     -- 560
+constant BIT_5 : integer range 0 to PROCESS_SIZE := BIT_4 + BIT_BAUD;     -- 660
+constant BIT_6 : integer range 0 to PROCESS_SIZE := BIT_5 + BIT_BAUD;     -- 760
+constant BIT_7 : integer range 0 to PROCESS_SIZE := BIT_6 + BIT_BAUD;     -- 860
+constant BIT_STOP : integer range 0 to PROCESS_SIZE := BIT_7 + BIT_BAUD;  -- 960
+constant BIT_NEXT : integer range 0 to PROCESS_SIZE := BIT_STOP + BIT_BAUD; -- 1060
 
 ---------------------------------------------------------------------------------------------------
 -- Signals
@@ -66,7 +69,7 @@ signal fpga_uart_rx_d1 : std_logic := '0';
 signal fpga_uart_rx_d2 : std_logic := '0';
 
 signal edge_detected : std_logic := '0';
-signal edge_timer : std_logic_vector(7 downto 0) := (others => '0');
+signal edge_timer : std_logic_vector(9 downto 0) := (others => '0');
 signal edge_error_correction : integer range 0 to PROCESS_SIZE := 0;
 
 signal uart_fifo_data_I : std_logic_vector(7 downto 0) := (others => '0');
@@ -263,7 +266,7 @@ begin
                         ---------------------------------------------------------------------------------------------------
                         if symbol_process_timer > BIT_START then
                             edge_detected <= '1';
-                            if to_integer(unsigned(edge_timer)) > 23 then
+                            if to_integer(unsigned(edge_timer)) > (BIT_BAUD - EDGE_COUNT_OFFSET) then
                                 edge_error_correction <= (edge_error_correction + ((to_integer(unsigned(edge_timer)) + EDGE_COUNT_OFFSET) mod BIT_BAUD));
                             end if;
                         end if;
